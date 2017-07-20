90ce2297d9a766cf7c91519b28ec527b22c236d2
Olivier Breuleux <breuleux@gmail.com>
2017-07-20 16:56:40 -0400

    Fix A-Normal conversion so that Let body is always a leaf node.

--- myia/compile.py
+++ myia/compile.py
@@ -4,11 +4,19 @@ from myia.front import GenSym
 from uuid import uuid4 as uuid
 
 
+# TODO: use the original function's gensym
+# TODO: Lambda should never be a sub-expression, it should be pulled
+#       out to the top-level, and wrapped with Closure if needed.
+
+
 def a_normal(node):
     """
     transform the expression represented by this node in
     A-normal form (ANF). ANF forbids expression nesting,
-    so e.g. (f (g x)) would become (let ((tmp (g x))) (f tmp))
+    so e.g. (f (g x)) would become:
+    (let ((tmp (g x))
+          (out (f tmp)))
+      out)
 
     :param node: The expression to process
     :return: The expression in A-normal form
@@ -31,7 +39,7 @@ class ANormalTransformer(Transformer):
         return result
 
     def transform_arguments(self, args, constructor,
-                          stash, base_name=None, tags=None):
+                            stash, base_name=None, tags=None):
 
         bindings = []
         new_args = []
@@ -42,6 +50,8 @@ class ANormalTransformer(Transformer):
             new_args.append(fn)
             if "/" in fn.label:
                 base_name = ""
+            elif fn.label.startswith('#'):
+                base_name = fn.label
             else:
                 base_name = re.split("[^A-Za-z_]", fn.label)[0]
 
@@ -52,16 +62,22 @@ class ANormalTransformer(Transformer):
             if tag is False:
                 new_arg = self.transform(arg)
             else:
-                new_arg = self.transform(arg, stash=(base_name + '/' + tag, bindings))
+                input_name = f'{base_name}/{tag}'
+                new_arg = self.transform(arg, stash=(input_name, bindings))
             new_args.append(new_arg)
 
         app = constructor(*new_args)
-        if bindings:
-            result = Let(bindings, app)
-        else:
-            result = app
-        return self.stash(stash, result, base_name + '/out')
 
+        if stash is False:
+            sym = self.stash((None, bindings), app, base_name + '/out')
+            result = Let(bindings, sym)
+        else:
+            if bindings:
+                result = Let(bindings, app)
+            else:
+                result = app
+
+        return self.stash(stash, result, base_name + '/out')
 
     def transform_Apply(self, node, stash=False):
         return self.transform_arguments((node.fn,) + node.args, Apply, stash)
@@ -78,7 +94,8 @@ class ANormalTransformer(Transformer):
 
     def transform_If(self, node, stash=False):
         return self.transform_arguments((node.cond, node.t, node.f),
-                                      If, stash, 'if', ('cond', False, False))
+                                        If, stash, 'if',
+                                        ('cond', 'then', 'else'))
 
     def transform_Let(self, node, stash=False):
         result = Let([(s, self.transform(b)) for s, b in node.bindings],
@@ -86,8 +103,12 @@ class ANormalTransformer(Transformer):
         return self.stash(stash, result, 'if')
 
     def transform_Tuple(self, node, stash=False):
-        def _Tuple(*args): return Tuple(args)
+        def _Tuple(*args):
+            return Tuple(args)
         return self.transform_arguments(node.values, _Tuple, stash, 'tup')
+
+    def transform_Closure(self, node, stash=False):
+        return node
 
 
 class CollapseLet(Transformer):
@@ -117,11 +138,16 @@ class CollapseLet(Transformer):
         return Lambda('#lambda', node.args, self.transform(node.body))
 
     def transform_Apply(self, node):
-        return Apply(self.transform(node.fn), *[self.transform(a) for a in node.args])
+        return Apply(self.transform(node.fn),
+                     *[self.transform(a) for a in node.args])
 
     def transform_Tuple(self, node):
         return Tuple(self.transform(a) for a in node.values)
 
     def transform_If(self, node):
-        return If(self.transform(node.cond), self.transform(node.t), self.transform(node.f))
+        return If(self.transform(node.cond),
+                  self.transform(node.t),
+                  self.transform(node.f))
 
+    def transform_Closure(self, node):
+        return node
