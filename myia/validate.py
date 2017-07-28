d57b43f9bb7a654f61764dd8c90995f1d17a8d5b
Olivier Breuleux <breuleux@gmail.com>
2017-07-28 16:25:15 -0400

    Implement more generic gradient testing with GradTester.

--- myia/validate.py
+++ myia/validate.py
@@ -84,6 +84,70 @@ def finite_diff(fn, args, eps=eps):
     return rval
 
 
+class GradTester:
+
+    def __init__(self, fn, gfn, args, out, argnames, outnames=None):
+        self.fn = fn
+        self.gfn = gfn
+        self.args = args
+        self.argnames = argnames
+        outname = fn.__name__
+        if isinstance(out, tuple):
+            self.outnames = tuple(f'{outname}_{i+1}' for i in range(len(out)))
+            self.out = out
+            self.wrap = lambda x: x
+            self.unwrap = lambda x: x
+        else:
+            self.outnames = (outname,)
+            self.out = (out,)
+            self.wrap = lambda x: (x,)
+            self.unwrap = lambda x: x[0]
+        if any(not isinstance(x, (int, float)) for x in self.out):
+            raise TypeError('Can only compute gradient'
+                            ' for a tuple of outputs.')
+        self.nin = len(self.argnames)
+        self.nout = len(self.outnames)
+
+    def compute_exact(self):
+        results = {}
+        for i, outname in enumerate(self.outnames):
+            out_sen = tuple((1 if k == i else 0) for k in range(self.nout))
+            grads = self.gfn(self.unwrap(out_sen))[1:]
+            for j, argname in enumerate(self.argnames):
+                results[f'd{outname}/d{argname}'] = grads[j]
+        self.exact = results
+        return results
+
+    def compute_finite_diff(self):
+        results = {}
+        for i, argname in enumerate(self.argnames):
+            argsl = list(self.args)
+            argsl[i] -= eps
+            tup_r1 = self.wrap(self.fn(*argsl))
+            argsl[i] += 2 * eps
+            tup_r2 = self.wrap(self.fn(*argsl))
+            d = tuple((r2 - r1) / (2 * eps) for r1, r2 in zip(tup_r1, tup_r2))
+            for j, outname in enumerate(self.outnames):
+                results[f'd{outname}/d{argname}'] = d[j]
+        self.finite_diff = results
+        return results
+
+    def compare(self):
+        exact = self.compute_exact()
+        fin = self.compute_finite_diff()
+        results = {}
+        for k in exact:
+            e = exact[k]
+            f = fin[k]
+            threshold = max(abs(rel_error * e), abs(rel_error * f))
+            results[k] = dict(
+                exact = e,
+                difference = f,
+                match = abs(e - f) <= threshold
+            )
+        return results
+
+
 def grad_test(data):
 
     pyfn, lbda, func, r, bindings = get_functions(data)
@@ -102,25 +166,15 @@ def grad_test(data):
 
     def test(args):
         python = guard(pyfn, args)
-        assert isinstance(python, (int, float))
         myia = guard(func, args)
         myiag, bprop = gfunc(*args)
-        grads = bprop(one(myiag))[1:]
-        diffs = finite_diff(pyfn, args)
-        derivatives = {}
-        for arg, g, d in zip(func.argnames, grads, diffs):
-            threshold = max(abs(rel_error * d), abs(rel_error * g))
-            derivatives[arg] = dict(
-                difference = d,
-                computed = g,
-                match = abs(d - g) <= threshold
-            )
+        gt = GradTester(pyfn, bprop, args, myiag, func.argnames, None)
         return dict(
             python = python,
             myia = myia,
             myiag = myiag,
             match = python == myia == myiag,
-            derivatives = derivatives
+            derivatives = gt.compare()
         )
 
     return dict(
