7933a2dec4c8ca294010db761bde30a228dc6aa7
Olivier Breuleux <breuleux@gmail.com>
2017-08-02 17:52:10 -0400

    Open buche channels lazily

--- myia/buche.py
+++ myia/buche.py
@@ -58,6 +58,7 @@ class Buche:
     def __init__(self, master, channel):
         self.master = master
         self.channel = channel
+        self.to_open = {}
 
     def raw(self, path=None, **params):
         if path is None:
@@ -67,14 +68,21 @@ class Buche:
         self.master.raw(path=self.channel, **params)
 
     def open(self, name, type, **params):
-        subchannel = self.join_path(name)
-        self.master.raw(command='open', path=subchannel, type=type, **params)
+        self.to_open[name] = (type, params)
 
     def join_path(self, p):
         return f'{self.channel.rstrip("/")}/{p.rstrip("/")}'
 
     def __getitem__(self, item):
         subchannel = self.join_path(item)
+        info = self.to_open.get(item, None)
+        if info:
+            del self.to_open[item]
+            type, params = info
+            self.master.raw(command='open',
+                            path=subchannel,
+                            type=type,
+                            **params)
         return Buche(self.master, subchannel)
 
     def __call__(self, obj, **params):
