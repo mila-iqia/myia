2687fbd10c7fd8bf101f7a258cce4a2b07c77a0c
Olivier Breuleux <breuleux@gmail.com>
2017-08-31 14:30:22 -0400

    Multiple tracks for DFA, type track.

--- myia/impl/flow_all.py
+++ myia/impl/flow_all.py
@@ -1,24 +1,59 @@
 
 from .main import symbol_associator, impl_bank
-from ..stx import Value
+from ..stx import Value, Tuple
 from ..util import Keyword
 from unification import var
-from ..inference.types import Array
+from ..inference.types import Array, Bool, Float64, Int64, Type
 
 
 ANY = Keyword('ANY')
 
 
+impl_bank['flow'] = {
+    'value': {},
+    'type': {}
+}
+
+
 @symbol_associator('flow')
 def impl_flow(sym, name, fn):
-    impl_bank['flow'][sym] = fn
+    impl_bank['flow']['value'][sym] = fn
+    return fn
+
+
+@symbol_associator('flow_type')
+def impl_flow_type(sym, name, fn):
+    def flow(dfa, node):
+        ttrack = dfa.tracks['type']
+
+        # TODO: this is basically redoing a brute force test
+        # every time, it's super inefficient
+        def signature_present(sig):
+            ins, out = sig
+            for i, arg in zip(ins, node.args):
+                if i not in dfa.values[ttrack][arg]:
+                    break
+            else:
+                return out
+            return False
+
+        for arg in node.args:
+            @dfa.on_flow_from(arg)
+            def on_arg(track, value):
+                if isinstance(value, Type):
+                    for sig in fn():
+                        out = signature_present(sig)
+                        if out:
+                            dfa.propagate(node, ttrack, out)
+
+    impl_bank['flow']['type'][sym] = flow
     return fn
 
 
 @impl_flow
 def flow_switch(dfa, node):
-    @dfa.on_flow_from(node.args[0])
-    def on_cond(value):
+    @dfa.on_flow_from(node.args[0], 'value')
+    def on_cond(track, value):
         if value == Value(True):
             dfa.flow_to(node.args[1], node)
         elif value == Value(False):
@@ -30,32 +65,50 @@ def flow_switch(dfa, node):
             raise TypeError('Condition for switch is not boolean.')
 
 
-# def std_type(nargs):
-#     T = var()
-#     return ([Array[T] for _ in range(nargs)], Array[T])
+@impl_flow
+def flow_index(dfa, node):
+    indexes = set()
+    tups = set()
+    track = dfa.value_track
+    dat, idx = node.args
+
+    @dfa.on_flow_from(idx, track)
+    def on_index(track, value):
+        if isinstance(value, Value):
+            vidx = value.value
+            indexes.add(vidx)
+            for tup in tups:
+                dfa.flow_to(tup.values[vidx], node)
+
+    @dfa.on_flow_from(dat, track)
+    def on_data(track, value):
+        if isinstance(value, Tuple):
+            tups.add(value)
+            for vidx in indexes:
+                dfa.flow_to(value.values[vidx], node)
 
 
-# @impl_flow_type
-# def type_add():
-#     return std_type(2)
+def std_type(nargs):
+    # T = var()
+    # return ([Array[T] for _ in range(nargs)], Array[T])
+    return [((T,) * nargs, T) for T in [Float64, Int64]]
 
 
-# @impl_flow_type
-# def type_dot():
-#     return std_type(2)
+@impl_flow_type
+def flow_type_add():
+    return std_type(2)
 
 
-# @impl_flow_type
-# def type_equal():
-#     T = var()
-#     return ((T, T), Bool)
+@impl_flow_type
+def flow_type_dot():
+    return std_type(2)
 
 
-# @impl_flow_type
-# def type_switch():
-#     T = var()
-#     return ((Bool, T, T), T)
+@impl_flow_type
+def flow_type_less():
+    return [((Float64, Float64), Bool),
+            ((Int64, Int64), Bool)]
 
 
 def default_flow(dfa, node):
-    dfa.add_value(node, ANY)
+    dfa.propagate(node, 'value', ANY)
--- myia/inference/dfa.py
+++ myia/inference/dfa.py
@@ -5,34 +5,60 @@ from collections import defaultdict
 from ..impl.flow_all import default_flow, ANY
 from ..impl.main import impl_bank
 from ..symbols import builtins
+from .types import Int64
 
 
 class DFA:
-    def __init__(self, genv):
+    def __init__(self, tracks, genv):
+        tracks = [track(self) for track in tracks]
+        self.tracks = {track.name: track for track in tracks}
+        self.value_track = self.tracks['value']
         self.genv = genv
         self.flow_graph = {}
-        self.values = defaultdict(set)
+        self.values = {track: defaultdict(set)
+                       for track in self.tracks.values()}
+
+    def propagate(self, node, track, value):
+        if isinstance(track, str):
+            track = self.tracks[track]
+        vals = self.values[track]
+        if value not in vals[node]:
+            vals[node].add(value)
+            self.flow_graph[node](track, value)
+
+    def propagate_value(self, node, value):
+        self.propagate(node, self.value_track, value)
+
+    def run_flows(self, method, *args):
+        for track in self.tracks.values():
+            getattr(track, f'flow_{method}')(*args)
 
     def flow_to(self, a, b):
         @self.on_flow_from(a)
-        def flow(value):
-            self.add_value(b, value)
+        def flow(track, value):
+            self.propagate(b, track, value)
 
-    def on_flow_from(self, node):
+    def on_flow_from(self, node, require_track=None):
+        if isinstance(require_track, str):
+            require_track = self.tracks[require_track]
+
         def deco(fn):
             reg = self.flow_graph[node].register
 
             @reg
-            def flow(_, value):
-                fn(value)
-            for v in self.values[node]:
-                flow(None, v)
+            def flow(_, track, value):
+                if not require_track or track is require_track:
+                    fn(track, value)
+            for track in self.tracks.values():
+                for v in self.values[track][node]:
+                    flow(None, track, v)
         return deco
 
     def on_function_flow(self, fn, node, flow_body=True, closure_args=0):
         args = node.args
 
-        def on_new_fn_value(new_value):
+        def on_new_fn_value(track, new_value):
+            assert track is self.value_track
             if isinstance(new_value, Lambda):
                 if flow_body:
                     self.flow_to(new_value.body, node)
@@ -44,20 +70,14 @@ class DFA:
                 self.on_function_flow(new_value.fn, node, flow_body, nargs)
             elif new_value in builtins.__dict__.values():
                 if flow_body:
-                    flow = impl_bank['flow'].get(new_value, default_flow)
-                    flow(self, node)
+                    self.run_flows('prim', new_value, node)
             elif new_value is ANY:
-                self.add_value(node, ANY)
+                self.propagate_value(node, ANY)
             else:
                 raise Exception(
                     f'Cannot flow a non-function here: {new_value}'
                 )
-        self.on_flow_from(fn)(on_new_fn_value)
-
-    def add_value(self, node, value):
-        if value not in self.values[node]:
-            self.values[node].add(value)
-            self.flow_graph[node](value)
+        self.on_flow_from(fn, self.value_track)(on_new_fn_value)
 
     def visit(self, node):
         if node in self.flow_graph:
@@ -72,7 +92,6 @@ class DFA:
         # (f a)
         # If (lambda (x) body) ~> f and v ~> a, v ~> x
         # If (lambda (x) body) ~> f and v ~> body, v ~> (f a)
-
         flow = self.flow_graph[node]
         self.visit(node.fn)
         for a in node.args:
@@ -88,13 +107,13 @@ class DFA:
         for arg in node.args:
             self.visit(arg)
         self.on_function_flow(node.fn, node, False, 0)
-        self.add_value(node, node)
+        self.propagate_value(node, node)
 
     def visit_Lambda(self, node):
         for arg in node.args:
             self.visit(arg)
         self.visit(node.body)
-        self.add_value(node, node)
+        self.propagate_value(node, node)
 
     def visit_Let(self, node):
         def _visit(v):
@@ -115,16 +134,14 @@ class DFA:
 
         def _bind(var, value):
             if isinstance(var, Tuple):
-                all_vars = _vars(var)
-
                 @self.on_flow_from(value)
-                def flow_tuple(new_value):
+                def flow_tuple(track, new_value):
                     if isinstance(new_value, Tuple):
                         for v, sub_value in zip(var.values, new_value.values):
                             _bind(v, sub_value)
                     else:
-                        for v in all_vars:
-                            self.add_value(v, ANY)
+                        for v in _vars(var):
+                            self.propagate_value(v, ANY)
             else:
                 self.flow_to(value, var)
 
@@ -139,14 +156,86 @@ class DFA:
         if node in self.genv.bindings:
             b = self.genv[node]
             self.visit(b)
-            self.add_value(node, b)
+            self.propagate_value(node, b)
         elif node in builtins.__dict__.values():
-            self.add_value(node, node)
+            self.propagate_value(node, node)
 
     def visit_Tuple(self, node):
         for v in node.values:
             self.visit(v)
-        self.add_value(node, node)
+        self.run_flows('Tuple', node)
 
     def visit_Value(self, node):
-        self.add_value(node, node)
+        self.run_flows('Value', node)
+
+
+class Track:
+    def __init__(self, name, dfa):
+        self.name = name
+        self.dfa = dfa
+
+    def propagate(self, node, value):
+        self.dfa.propagate(node, self, value)
+
+    def flow_Apply(self, node):
+        pass
+
+    def flow_Begin(self, node):
+        pass
+
+    def flow_Closure(self, node):
+        pass
+
+    def flow_Lambda(self, node):
+        pass
+
+    def flow_Let(self, node):
+        pass
+
+    def flow_Tuple(self, node):
+        pass
+
+    def flow_Symbol(self, node):
+        pass
+
+    def flow_Value(self, node):
+        pass
+
+    def flow_prim(self, prim, node):
+        impls = impl_bank['flow'][self.name]
+        flow = impls.get(prim, self.flow_default)
+        flow(self.dfa, node)
+
+    def flow_default(self, prim, node):
+        pass
+
+    def __str__(self):
+        return f'<Track:{self.name}>'
+
+
+class ValueTrack(Track):
+    def __init__(self, dfa):
+        super().__init__('value', dfa)
+
+    def flow_Tuple(self, node):
+        self.propagate(node, node)
+
+    def flow_Value(self, node):
+        self.propagate(node, node)
+
+    def flow_default(self, _, node):
+        self.propagate(node, ANY)
+
+
+class TypeTrack(Track):
+    def __init__(self, dfa):
+        super().__init__('type', dfa)
+
+    def flow_Value(self, node):
+        if isinstance(node.value, int):
+            self.propagate(node, Int64)
+        elif isinstance(node.value, float):
+            self.propagate(node, Float64)
+
+    def flow_default(self, _, node):
+        self.propagate(node, ANY)
