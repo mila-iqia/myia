46c03e7f20531b715028f2cebbaa30d9231d86a2
Olivier Breuleux <breuleux@gmail.com>
2017-11-28 21:38:12 -0500

    Add ClosureUnconversionPass

--- myia/ir/opt.py
+++ myia/ir/opt.py
@@ -1,6 +1,11 @@
 
-from ..lib import BackedUniverse, is_struct, StructuralMap
-from .graph import IRGraph
+from ..lib import BackedUniverse, is_struct, StructuralMap, Primitive
+from .graph import IRGraph, IRNode
+from ..symbols import builtins
+from ..stx import GenSym, is_builtin
+
+
+ogen = GenSym('::opt')
 
 
 class OptimizedUniverse(BackedUniverse):
@@ -22,3 +27,42 @@ class OptimizedUniverse(BackedUniverse):
     def optimize(self, graph):
         for passs in self.passes:
             passs(self, graph)
+
+
+class ClosureUnconversionPass:
+
+    def __call__(self, universe, graph):
+        cpru = universe.universes['const_prop']
+        pool = {graph}
+        while pool:
+            g = pool.pop()
+            for node in g.iternodes():
+                if node.fn and node.fn.value is builtins.partial:
+                    fnn = node.inputs[0]
+                    fn = fnn.value
+                    if isinstance(fn, IRGraph):
+                        g2 = fn
+                        g.replace(node, node.inputs[0])
+                        for i, g2i in zip(node.inputs[1:], g2.inputs):
+                            g2.replace(g2i, i)
+                        g2.inputs = g2.inputs[len(node.inputs) - 1:]
+                        pool.add(g2)
+                    elif is_builtin(fn):
+                        sym = fn
+                        fn = cpru[sym]
+                        if isinstance(fn, Primitive):
+                            g2 = IRGraph(None, ogen(sym, '~'), g.gen)
+                            clins = node.inputs[1:]
+                            argins = [IRNode(g2, ogen(argn))
+                                      for argn in fn.argnames[len(clins):]]
+                            g2.inputs = clins + argins
+                            o = IRNode(g2, ogen('/out'))
+                            g2.output = o
+                            o.set_app(fnn, g2.inputs)
+                            g2n = IRNode(None, g2.tag)
+                            g2n.value = g2
+                            g.replace(node, g2n)
+                elif isinstance(node.value, IRGraph):
+                    pool.add(node.value)
+                elif node.fn and isinstance(node.fn.value, IRGraph):
+                    pool.add(node.fn.value)
