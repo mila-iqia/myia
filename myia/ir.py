ed6ac9455f56ff8ef9b994cf981f22a8c837425f
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2017-12-15 12:13:51 -0500

    Refactoring IR
    * Added a Return type node.
    * Added parameters to the graph object.
    * Allow graph to be constructed without output node.
    * Renamed 'output' attribute to return_ attribute to harmonize
    * terminology.
    * Changed 'uses' attributed to tuples of nodes and indices.
    * Pass arguments to Node class instead of overriding attributes.

--- myia/ir.py
+++ myia/ir.py
@@ -9,54 +9,67 @@ implicitly creates a nested function. Functions are first-class objects, so
 returning a nested function creates a closure.
 
 """
-from typing import List, Set, Dict, Any, Optional
+from typing import List, Set, Tuple, Dict, Any, Optional
 
 from myia.utils import Named
 
 PARAMETER = Named('PARAMETER')
+RETURN = Named('RETURN')
 
 
 class Graph:
     """A function graph.
 
     Attributes:
-        output: The `Node` whose value will be returned by this function.
+        parameters: The parameters of this function as a list of `Parameter`
+            nodes. Parameter nodes that are unreachable by walking from the
+            output node correspond to unused parameters.
+        return_: The `Return` node whose value will be returned by this
+            function. A graph initially has no output node (because it won't be
+            known e.g. until the function has completed parsing), but it must
+            be set afterwards for the graph instance to be valid.
 
     """
 
-    def __init__(self, output: 'Node') -> None:
+    def __init__(self) -> None:
         """Construct a graph."""
-        self.output = output
+        self.parameters: List[Parameter] = []
+        self.return_: Optional[Return] = None
         self.debug: Dict = {}
 
 
 class Node:
     """A node in the graph-based ANF IR.
 
-    There are three types of nodes: Function applications; parameters; and
-    constants such as numbers and functions.
+    There are four types of nodes: Function applications; parameters; return
+    values; and constants such as numbers and functions.
 
     Attributes:
         inputs: If the node is a function application, the first node input is
-            the function to apply, followed by the arguments.
-        uses: A set of function applications that use this node as a function
-            or argument. This attribute is optional and initially set to
-            `None`.
+            the function to apply, followed by the arguments. These are use-def
+            edges.
         value: The value of this node, if it is a constant. Parameters have the
             special value `PARAMETER`.
         graph: The function definition graph that this node belongs to for
             values and parameters.
+        uses: A set of tuples with the nodes that use this node alongside with
+            the index. These def-use edges are the reverse of the `inputs`
+            attribute, creating a doubly linked graph structure. This attribute
+            is optional and initially set to `None`. If this attribute is
+            present, care must be taken to make sure that changes to `uses` is
+            reflected in the other nodes' `inputs` attribute and vice versa.
         debug: A dictionary with debug information about this node e.g. a
             human-readable name and the Python source code.
 
     """
 
-    def __init__(self) -> None:
+    def __init__(self, inputs: List['Node'], value: Any,
+                 graph: Optional[Graph]) -> None:
         """Construct a node."""
-        self.inputs: List[Node] = []
-        self.uses: Optional[Set[Node]] = None
-        self.value: Any = None
-        self.graph: Graph = None
+        self.inputs = inputs
+        self.value = value
+        self.graph = graph
+        self.uses: Optional[Set[Tuple[Node, int]]] = None
         self.debug: Dict = {}
 
 
@@ -71,9 +84,7 @@ class Apply(Node):
         """Construct an application."""
         if len(inputs) < 1:
             raise ValueError("at least one input must be provided")
-        super().__init__()
-        self.inputs = inputs
-        self.graph = graph
+        super().__init__(inputs, None, graph)
 
 
 class Parameter(Node):
@@ -87,9 +98,20 @@ class Parameter(Node):
 
     def __init__(self, graph: Graph) -> None:
         """Construct the parameter."""
-        super().__init__()
-        self.value = PARAMETER
-        self.graph = graph
+        super().__init__([], PARAMETER, graph)
+
+
+class Return(Node):
+    """The value returned by a function.
+
+    Return nodes have exactly one input, which points to the value that the
+    function will return. They are a root node in the function graph.
+
+    """
+
+    def __init__(self, input_: Node, graph: Graph) -> None:
+        """Construct a return node."""
+        super().__init__([input_], RETURN, graph)
 
 
 class Constant(Node):
@@ -108,5 +130,4 @@ class Constant(Node):
 
     def __init__(self, value: Any) -> None:
         """Construct a literal."""
-        super().__init__()
-        self.value = value
+        super().__init__([], value, None)
