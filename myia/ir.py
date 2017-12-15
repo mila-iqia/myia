e6e8da181ee0818749b3c013ddd31583ae058a39
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2017-12-15 16:19:22 -0500

    Doubly linked graph structure
    Create a custom container for the inputs so that changes to the inputs
    are automatically updates the def-use edges as well.

--- myia/ir.py
+++ myia/ir.py
@@ -9,7 +9,8 @@ implicitly creates a nested function. Functions are first-class objects, so
 returning a nested function creates a closure.
 
 """
-from typing import List, Set, Tuple, Dict, Any, Optional
+from typing import (List, Set, Tuple, Dict, Any, Optional, Sequence,
+                    MutableSequence, overload, Iterable)
 
 from myia.utils import Named
 
@@ -54,10 +55,7 @@ class Node:
             values and parameters.
         uses: A set of tuples with the nodes that use this node alongside with
             the index. These def-use edges are the reverse of the `inputs`
-            attribute, creating a doubly linked graph structure. This attribute
-            is optional and initially set to `None`. If this attribute is
-            present, care must be taken to make sure that changes to `uses` is
-            reflected in the other nodes' `inputs` attribute and vice versa.
+            attribute, creating a doubly linked graph structure.
         debug: A dictionary with debug information about this node e.g. a
             human-readable name and the Python source code.
 
@@ -66,11 +64,102 @@ class Node:
     def __init__(self, inputs: List['Node'], value: Any,
                  graph: Optional[Graph]) -> None:
         """Construct a node."""
-        self.inputs = inputs
+        self.inputs = Inputs(self, inputs)
         self.value = value
         self.graph = graph
-        self.uses: Optional[Set[Tuple[Node, int]]] = None
+        self.uses: Set[Tuple[Node, int]] = set()
         self.debug: Dict = {}
+
+
+class Inputs(MutableSequence[Node]):
+    """Container data structure for node inputs.
+
+    This mutable sequence data structure can be used to keep track of a node's
+    inputs. Any insertion or deletion of edges will be reflected in the inputs'
+    `uses` attribute.
+
+    """
+
+    def __init__(self, node: Node, initlist: Iterable[Node] = None) -> None:
+        """Construct the inputs container for a node.
+
+        Args:
+            node: The node of which the inputs are stored.
+            initlist: A sequence of nodes to initialize the container with.
+
+        """
+        self.node = node
+        self.data: List[Node] = []
+        if initlist is not None:
+            self.extend(initlist)
+
+    @overload
+    def __getitem__(self, index: int) -> Node:
+        pass
+
+    @overload  # noqa: F811
+    def __getitem__(self, s: slice) -> Sequence[Node]:
+        pass
+
+    def __getitem__(self, index):  # noqa: F811
+        """Get an input by its index."""
+        return self.data.__getitem__(index)
+
+    @overload
+    def __setitem__(self, index: int, value: Node) -> None:
+        pass
+
+    @overload  # noqa: F811
+    def __setitem__(self, index: slice, value: Iterable[Node]) -> None:
+        pass
+
+    def __setitem__(self, index, value):  # noqa: F811
+        """Replace an input with another."""
+        if isinstance(index, slice):
+            for i, v in zip(range(*index.indices(len(self))), value):
+                self[i] = v
+        old_value = self.data[index]
+        if old_value.uses is not None:
+            old_value.uses.remove((self.node, index))
+        if value.uses is not None:
+            value.uses.add((self.node, index))
+        self.data.__setitem__(index, value)
+
+    @overload
+    def __delitem__(self, index: int) -> None:
+        pass
+
+    @overload  # noqa: F811
+    def __delitem__(self, index: slice) -> None:
+        pass
+
+    def __delitem__(self, index):  # noqa: F811
+        """Delete an input."""
+        if isinstance(index, slice):
+            for i in range(*index.indices(len(self))):
+                del self[i]
+        value = self.data[index]
+        if value.uses is not None:
+            value.uses.remove((self.node, index))
+        self.data.__delitem__(index)
+
+    def __len__(self) -> int:
+        """Get the number of inputs."""
+        return len(self.data)
+
+    def insert(self, index: int, value: Node) -> None:
+        """Insert an input at a given location."""
+        for i, next_value in enumerate(reversed(self.data[index:])):
+            if next_value.uses is not None:
+                next_value.uses.remove((self.node, len(self) - i - 1))
+                next_value.uses.add((self.node, len(self) - i))
+        if value.uses is not None:
+            value.uses.add((self.node, index))
+        self.data.insert(index, value)
+
+    def __repr__(self) -> str:
+        """Return a string representation of the inputs."""
+        return f"Inputs({self.data})"
 
 
 class Apply(Node):
