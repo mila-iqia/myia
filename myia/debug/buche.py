db58cdd376a6ce5a13f8bcf2b1667dfed3532f5e
Olivier Breuleux <breuleux@gmail.com>
2018-01-25 12:01:55 -0500

    Fix buche.py

--- myia/debug/buche.py
+++ myia/debug/buche.py
@@ -1,4 +1,5 @@
+"""Provide a buche function that works on Myia objects."""
 
 # Load custom hrepr methods for Graph etc.
-from myia.debug import gprint
-from buche import buche
+from myia.debug import gprint  # noqa
+from buche import buche  # noqa
