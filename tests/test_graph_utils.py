0fb5fcf784d7100034b34ce092c99b5f60ae0539
Olivier Breuleux <breuleux@gmail.com>
2018-04-12 12:33:56 -0400

    Add include argument to toposort

--- tests/test_graph_utils.py
+++ tests/test_graph_utils.py
@@ -1,10 +1,14 @@
 """Test generic graph utilities."""
 
-from myia.graph_utils import dfs, toposort
+from myia.graph_utils import dfs, toposort, FOLLOW, NOFOLLOW, EXCLUDE
 
 
 def _succ(x):
     return reversed(x) if isinstance(x, tuple) else ()
+
+
+def _incl(x):
+    return FOLLOW
 
 
 def test_dfs():
@@ -29,22 +33,23 @@ def test_dfs_dups():
     assert order == [d, a, 1, 2, c, b, 3, 4, 5, 6]
 
 
-def _check_toposort(order, root, succ):
-    nodes = set(dfs(root, succ))
+def _check_toposort(order, root, succ=_succ, incl=_incl):
+    nodes = set(dfs(root, succ, incl))
     assert len(order) == len(nodes)
     for node in nodes:
         for i in succ(node):
-            assert order.index(i) < order.index(node)
+            if i in order:
+                assert order.index(i) < order.index(node)
 
 
 def test_toposort():
     a = (1, 2)
     b = (3, 4, 5)
-    c = (b, 6)
+    c = (b, a, 6)
     d = (a, c)
 
-    order = list(toposort(d, _succ))
-    _check_toposort(order, d, _succ)
+    order = list(toposort(d, _succ, _incl))
+    _check_toposort(order, d, _succ, _incl)
 
 
 def test_toposort_overlap():
@@ -52,5 +57,29 @@ def test_toposort_overlap():
     b = (a, 2)
     c = (b, a, 2)
 
-    order = list(toposort(c, _succ))
-    _check_toposort(order, c, _succ)
+    order = list(toposort(c, _succ, _incl))
+    _check_toposort(order, c, _succ, _incl)
+
+
+def test_toposort_incl():
+    def _incl_nf(x):
+        if isinstance(x, tuple) and len(x) == 2:
+            return NOFOLLOW
+        else:
+            return FOLLOW
+
+    def _incl_x(x):
+        if isinstance(x, tuple) and len(x) == 2:
+            return EXCLUDE
+        else:
+            return FOLLOW
+
+    a = (1, 2)
+    b = (a, 3)
+    c = (a, b, 4, 5)
+
+    order1 = list(toposort(c, _succ, _incl_nf))
+    _check_toposort(order1, c, _succ, _incl_nf)
+
+    order2 = list(toposort(c, _succ, _incl_x))
+    _check_toposort(order2, c, _succ, _incl_x)
