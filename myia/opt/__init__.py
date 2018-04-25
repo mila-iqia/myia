b172441563fce5a6c34aafb1692e5541cc6948a2
Olivier Breuleux <breuleux@gmail.com>
2018-04-25 13:24:15 -0400

    Use __visit__ instead of GraphUnification

--- myia/opt/__init__.py
+++ myia/opt/__init__.py
@@ -2,7 +2,7 @@
 
 from .opt import (  # noqa
     VarNode, sexp_to_node, sexp_to_graph,
-    GraphUnification, PatternSubstitutionOptimization,
+    PatternSubstitutionOptimization,
     pattern_replacer,
     PatternOptimizerSinglePass, PatternOptimizerEquilibrium
 )
