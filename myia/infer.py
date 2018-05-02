c8b529a9eed8670fe5433cb402a5f92f008e7bed
Olivier Breuleux <breuleux@gmail.com>
2018-05-02 17:00:22 -0400

    Working InferenceEngine + tests

---
+++ myia/infer.py
@@ -0,0 +1,507 @@
+
+import asyncio
+
+from .dtype import Int, Bool, Float, Tuple, List
+from .prim import ops as P, Primitive
+from .ir import Graph, Constant, \
+    is_constant, is_constant_graph, is_apply, is_parameter
+from .utils import Named, Registry
+from .cconv import NestingAnalyzer
+from .prim.py_implementations import implementations as pyimpl
+
+
+ANYTHING = Named('ANYTHING')
+
+
+class MyiaTypeError(Exception):
+    pass
+
+
+def typeof(v):
+    if isinstance(v, bool):
+        return Bool()
+    elif isinstance(v, int):
+        return Int(64)
+    elif isinstance(v, float):
+        return Float(64)
+    elif isinstance(v, tuple):
+        return Tuple(map(typeof, v))
+    else:
+        raise TypeError(f'Untypable value: {v}')
+
+
+####################
+# Inferrer classes #
+####################
+
+
+class Inferrer:
+    def __init__(self, engine):
+        self.engine = engine
+        self.cache = {}
+
+    async def __call__(self, *args):
+        if args not in self.cache:
+            self.cache[args] = await self.infer(*args)
+        return self.cache[args]
+
+
+class PrimitiveInferrer(Inferrer):
+    def __init__(self, engine, track, prim, inferrer_fn):
+        super().__init__(engine)
+        self.track = track
+        self.prim = prim
+        self.inferrer_fn = inferrer_fn
+
+    def infer(self, *args):
+        return self.inferrer_fn(self.engine, *args)
+
+    def provably_equivalent(self, other):
+        return isinstance(other, PrimitiveInferrer) \
+            and other.track == self.track \
+            and other.inferrer_fn == self.inferrer_fn
+
+
+class GraphInferrer(Inferrer):
+    def __init__(self, engine, track, graph, context):
+        super().__init__(engine)
+        self.track = track
+        self.graph = graph
+        self.context = context.filter(graph)
+
+    async def infer(self, *args):
+        engine = self.engine
+        argvals = {
+            track: await asyncio.gather(
+                *[engine.get(track, arg) for arg in args],
+                loop=engine.loop
+            )
+            for track in engine.tracks
+        }
+
+        gsigs = {(self.graph, track, tuple(argvals[track]))
+                 for track in engine.tracks}
+        context = self.context | gsigs
+
+        for track, vals in argvals.items():
+            for p, v in zip(self.graph.parameters, vals):
+                ref = Reference(p, context)
+                engine.cache_value(track, ref, v)
+
+        out = Reference(self.graph.output, context)
+        return await engine.get(self.track, out)
+
+    def provably_equivalent(self, other):
+        return isinstance(other, GraphInferrer) \
+            and other.track == self.track \
+            and other.graph == self.graph \
+            and other.context == self.context
+
+
+def primitive_inferrer(track, prim):
+    def wrap(fn):
+        p = lambda engine: PrimitiveInferrer(engine, track, prim, fn)
+        all_inferrers[track].register(prim)(p)
+        return p
+    return wrap
+
+
+def value_inferrer(prim):
+    async def infer(engine, *refs):
+        coros = [engine.get('value', ref) for ref in refs]
+        args = await asyncio.gather(*coros, loop=engine.loop)
+        if any(arg is ANYTHING for arg in args):
+            return ANYTHING
+        else:
+            return pyimpl[prim](*args)
+
+    return primitive_inferrer('value', prim)(infer)
+
+
+#############
+# Inferrers #
+#############
+
+
+all_inferrers = {
+    'value': Registry(),
+    'type': Registry()
+}
+
+
+@primitive_inferrer('value', Constant)
+async def infer_value_constant(engine, ct):
+    v = ct.node.value
+    if isinstance(v, Primitive):
+        vinfs = all_inferrers['value']
+        if v in vinfs:
+            return vinfs[v](engine)
+        else:
+            return value_inferrer(v)(engine)
+    elif isinstance(v, Graph):
+        return GraphInferrer(engine, 'value', v, ct.context)
+    else:
+        return v
+
+
+@primitive_inferrer('value', P.if_)
+async def infer_value_if(engine, cond, tb, fb):
+    v = await engine.get('value', cond)
+    if v is True:
+        fn = await engine.get('value', tb)
+    elif v is False:
+        fn = await engine.get('value', fb)
+    elif v is ANYTHING:
+        return ANYTHING
+
+    return await fn()
+
+
+@primitive_inferrer('type', Constant)
+async def infer_type_constant(engine, ct):
+    v = ct.node.value
+    if isinstance(v, Primitive):
+        return all_inferrers['type'][v](engine)
+    elif isinstance(v, Graph):
+        return GraphInferrer(engine, 'type', v, ct.context)
+    else:
+        return typeof(ct.node.value)
+
+
+@primitive_inferrer('type', P.if_)
+async def infer_type_if(engine, cond, tb, fb):
+    assert await engine.get('type', cond) == Bool()
+    v = await engine.get('value', cond)
+    tb_res = (await engine.get('type', tb))()
+    fb_res = (await engine.get('type', fb))()
+    if v is True:
+        return await tb_res
+    elif v is False:
+        return await fb_res
+    elif v is ANYTHING:
+        return await engine.force_same('type', tb_res, fb_res)
+
+
+@primitive_inferrer('type', P.cons_tuple)
+async def infer_type_cons_tuple(engine, x, y):
+    x_t = await engine.get('type', x)
+    y_t = await engine.get('type', y)
+    assert isinstance(y_t, Tuple)
+    return Tuple([x_t, *y_t.elements])
+
+
+@primitive_inferrer('type', P.getitem)
+async def infer_type_getitem(engine, seq, idx):
+    seq_t = await engine.get('type', seq)
+    idx_t = await engine.get('type', idx)
+    if not isinstance(idx_t, Int):
+        raise MyiaTypeError('Expected Int for index')
+
+    if isinstance(seq_t, Tuple):
+        idx_v = await engine.get('value', idx)
+        assert idx_v is not ANYTHING
+        return seq_t.elements[idx_v]
+    elif isinstance(seq_t, List):
+        return seq_t.element_type
+    else:
+        raise MyiaTypeError('Wrong seq type for getitem')
+
+
+async def infer_type_compare_bin(engine, x, y):
+    t = await engine.force_same('type', x, y)
+    if not isinstance(t, (Int, Float)):
+        raise MyiaTypeError('Expected number')
+    return Bool()
+
+
+async def infer_type_arith_bin(engine, x, y):
+    t = await engine.force_same('type', x, y)
+    if not isinstance(t, (Int, Float)):
+        raise MyiaTypeError('Expected number')
+    return t
+
+
+for op in [P.add, P.sub, P.mul]:
+    all_inferrers['type'].register(op)(
+        lambda engine: PrimitiveInferrer(
+            engine, 'type', op, infer_type_arith_bin
+        )
+    )
+
+for op in [P.lt, P.gt, P.eq, P.le, P.ge]:
+    all_inferrers['type'].register(op)(
+        lambda engine: PrimitiveInferrer(
+            engine, 'type', op, infer_type_compare_bin
+        )
+    )
+
+
+##############
+# References #
+##############
+
+
+class Context:
+    def __init__(self, engine, parts):
+        self.engine = engine
+        self.parts = frozenset(parts)
+
+    def filter(self, graph):
+        if graph is None:
+            return Context(self.engine, ())
+        deps = self.engine.deps[graph]
+        return Context(
+            self.engine,
+            ((g, track, argsig)
+             for (g, track, argsig) in self.parts
+             if g in deps or g is graph)
+        )
+
+    def __iter__(self):
+        return iter(self.parts)
+
+    def __hash__(self):
+        return hash((self.engine, self.parts))
+
+    def __eq__(self, other):
+        return type(other) is Context \
+            and self.engine == other.engine \
+            and self.parts == other.parts
+
+    def __or__(self, other):
+        return Context(self.engine, self.parts | other)
+
+
+class Reference:
+    def __init__(self, node, context):
+        self.node = node
+        g = node.value if is_constant_graph(node) else node.graph
+        self.context = context.filter(g)
+
+    def __eq__(self, other):
+        return isinstance(other, Reference) \
+            and self.node is other.node \
+            and self.context == other.context
+
+    def __hash__(self):
+        return hash((self.node, self.context))
+
+    def __hrepr__(self, H, hrepr):
+        return hrepr({'node': self.node, 'context': self.context})
+
+
+class VirtualReference:
+    def __init__(self, **values):
+        self.values = values
+
+
+########
+# Core #
+########
+
+
+class InferrerEquivalenceClass:
+
+    def __init__(self, engine, members):
+        self.engine = engine
+        self.checked_members = set()
+        self.pending_members = members
+        self.results = {}
+
+    def update(self, other):
+        self.pending_members.update(other.checked_members)
+        self.pending_members.update(other.pending_members)
+        self.pending_members -= self.checked_members
+
+    def __iter__(self):
+        return iter(self.checked_members | self.pending_members)
+
+    async def check(self):
+        maybe_changes = False
+
+        for inf in self.pending_members:
+            maybe_changes = True
+            for args, expected in self.results.items():
+                v = await inf(*args)
+                self.engine.equiv.declare_equivalent(expected, v)
+        self.checked_members.update(self.pending_members)
+        self.pending_members = set()
+
+        all_keys = set()
+        for m in self.checked_members:
+            all_keys.update(m.cache.keys())
+
+        to_check = all_keys - set(self.results.keys())
+
+        for args in to_check:
+            maybe_changes = True
+            inf1, *others = self.checked_members
+            res1 = inf1(*args)
+            self.results[args] = res1
+            for inf2 in others:
+                res2 = inf2(*args)
+                self.engine.equiv.declare_equivalent(res1, res2)
+
+        return maybe_changes
+
+
+class EquivalencePool:
+
+    def __init__(self, engine):
+        self.engine = engine
+        self.eqclasses = {}
+        self.pending = []
+        self.checked = {}
+
+    def declare_equivalent(self, x, y):
+        self.pending.append((x, y))
+        self.engine.schedule_function(self.check)
+
+    async def _process_equivalence(self, x, y):
+        vx = await x
+        vy = await y
+
+        if isinstance(vx, Inferrer) and isinstance(vy, Inferrer):
+            if vx.provably_equivalent(vy):
+                return
+
+            eqx = self.eqclasses.get(
+                vx, InferrerEquivalenceClass(self.engine, {vx})
+            )
+            eqy = self.eqclasses.get(
+                vy, InferrerEquivalenceClass(self.engine, {vy})
+            )
+            eqx.update(eqy)
+            for z in eqx:
+                self.eqclasses[z] = eqx
+
+        elif vx == vy:
+            pass
+
+        else:
+            self.engine.errors.add(
+                MyiaTypeError(f'Type mismatch: {vx} != {vy}')
+            )
+
+    async def check(self):
+        maybe_changes = False
+        pending, self.pending = self.pending, []
+        for x, y in pending:
+            await self._process_equivalence(x, y)
+            maybe_changes = True
+
+        for eq in set(self.eqclasses.values()):
+            maybe_changes |= await eq.check()
+
+        if maybe_changes:
+            self.engine.schedule_function(self.check)
+
+
+class EqEquivalence:
+    def __init__(self, engine):
+        self.engine = engine
+
+    def declare_equivalent(self, x, y):
+        self.engine.schedule(self.assert_equivalent(x, y))
+
+    async def assert_equivalent(self, x, y):
+        vx = await x
+        vy = await y
+        if not (vx == vy):
+            self.engine.errors.add(TypeError(f'Type mismatch: {vx} != {vy}'))
+
+
+class InferenceEngine:
+
+    def __init__(self, inferrers, eq_class=EquivalencePool):
+        self.loop = asyncio.get_event_loop()
+        self.tracks = tuple(inferrers.keys())
+        self.inferrers = inferrers
+        self.cache = {track: {} for track in self.tracks}
+        self.todo = set()
+        self.errors = set()
+        self.equiv = eq_class(self)
+
+    async def compute_ref(self, track, ref):
+        if isinstance(ref, VirtualReference):
+            return ref.values[track]
+        node = ref.node
+        ctx = ref.context
+        infs = self.inferrers[track]
+        if is_constant(node):
+            return await infs[Constant](self)(ref)
+        elif is_apply(node):
+            n_fn, *n_args = node.inputs
+            inf = await self.get(track, Reference(n_fn, ctx))
+            if inf is ANYTHING:
+                return ANYTHING
+            assert isinstance(inf, Inferrer)
+            argrefs = [Reference(node, ctx) for node in n_args]
+            return await inf(*argrefs)
+        else:
+            raise Exception(f'Cannot process: {node}')
+
+    def get(self, track, ref):
+        futs = self.cache[track]
+        if ref not in futs:
+            futs[ref] = self.loop.create_task(self.compute_ref(track, ref))
+        return futs[ref]
+
+    def cache_value(self, track, ref, value):
+        fut = asyncio.Future()
+        fut.set_result(value)
+        self.cache[track][ref] = fut
+
+    def schedule(self, coro):
+        self.todo.add(lambda: coro)
+
+    def schedule_function(self, fn):
+        self.todo.add(fn)
+
+    async def run(self, graph, args):
+        assert len(graph.parameters) == len(args)
+
+        ctx = Context(
+            self,
+            ((graph, track, tuple(arg[track] for arg in args))
+             for track in self.tracks)
+        )
+
+        for p, arg in zip(graph.parameters, args):
+            for track, value in arg.items():
+                ref = Reference(p, ctx)
+                self.cache_value(track, ref, value)
+
+        oref = Reference(graph.output, ctx)
+
+        for track in self.tracks:
+            self.schedule(self.get(track, oref))
+
+        while self.todo:
+            todo = list(self.todo)
+            self.todo = set()
+            todo = [t() for t in todo]
+            await asyncio.gather(*todo, loop=self.loop)
+
+        if self.errors:
+            raise self.errors.pop()
+        else:
+            return {track: self.cache[track][oref].result()
+                    for track in self.tracks}
+
+    def run_sync(self, graph, args):
+        self.deps = NestingAnalyzer(graph).graph_dependencies_total()
+        return self.loop.run_until_complete(self.run(graph, args))
+
+    async def force_same(self, track, *refs):
+        futs = [self.get(track, ref) if isinstance(ref, Reference) else ref
+                for ref in refs]
+        done, pending = await asyncio.wait(
+            futs,
+            loop=self.loop,
+            return_when=asyncio.FIRST_COMPLETED
+        )
+        main = done.pop()
+        for fut in done|pending:
+            self.equiv.declare_equivalent(fut, main)
+        return main.result()
---
+++ tests/test_infer.py
@@ -0,0 +1,429 @@
+
+from pytest import mark
+
+from myia.api import parse
+from myia.infer import \
+    InferenceEngine, typeof, ANYTHING, all_inferrers, MyiaTypeError
+
+from myia.dtype import Bool, Int, Float, Tuple as T, List as L
+from myia.prim.py_implementations import add, mul, lt
+
+
+B = Bool()
+
+i16 = Int(16)
+i32 = Int(32)
+i64 = Int(64)
+
+f16 = Float(16)
+f32 = Float(32)
+f64 = Float(64)
+
+li16 = L(Int(16))
+li32 = L(Int(32))
+li64 = L(Int(64))
+
+lf16 = L(Float(16))
+lf32 = L(Float(32))
+lf64 = L(Float(64))
+
+
+def fill_in(test, tracks):
+    for entry in test:
+        if entry is TypeError:
+            continue
+        for track in tracks:
+            if track in entry:
+                continue
+            elif track == 'type':
+                entry[track] = typeof(entry['value'])
+            elif track == 'value':
+                entry[track] = ANYTHING
+    return test
+
+
+def infer(**tests_spec):
+
+    tests = []
+    all_tracks = ['type', 'value']
+
+    for main_track, ts in tests_spec.items():
+        if not isinstance(ts, list):
+            ts = [ts]
+        for t in ts:
+            test = []
+            for entry in t:
+                if entry is TypeError:
+                    test.append(TypeError)
+                elif isinstance(entry, dict):
+                    test.append(entry)
+                else:
+                    test.append({main_track: entry})
+            tests.append(fill_in(test, all_tracks))
+
+    def decorate(fn):
+        def run_test(spec):
+            *args, expected_out = spec
+            g = parse(fn)
+            inferrer = InferenceEngine(all_inferrers)
+            try:
+                out = inferrer.run_sync(g, args)
+            except MyiaTypeError as e:
+                out = TypeError
+            assert out == expected_out
+
+        m = mark.parametrize('spec', list(tests))(run_test)
+        m.__orig__ = fn
+        return m
+
+    return decorate
+
+
+type_signature_arith_bin = [
+    (i64, i64, i64),
+    (f64, f64, f64),
+    (i64, f64, TypeError)
+]
+
+
+@infer(type=type_signature_arith_bin)
+def test_prim_mul(x, y):
+    return x * y
+
+
+@infer(type=type_signature_arith_bin)
+def test_if(x, y):
+    if x > y:
+        return x
+    else:
+        return y
+
+
+@infer(
+    type=[
+        (i64, i64, i64),
+        (i64, f64, f64),
+        (f64, f64, TypeError)
+    ]
+)
+def test_while(x, y):
+    rval = y
+    while x > 0:
+        rval = rval * y
+        x = x - 1
+    return rval
+
+
+@infer(type=(i64, f64, T(i64, f64)))
+def test_tup(x, y):
+    return (x, y)
+
+
+@infer(type=[(i64, f64, i64), (f64, i64, f64)])
+def test_getitem(x, y):
+    return (x, y)[0]
+
+
+@infer(type=(i64, f64, T(i64, f64)))
+def test_multitype_function(x, y):
+    def mul(a, b):
+        return a * b
+    return (mul(x, x), mul(y, y))
+
+
+@infer(type=type_signature_arith_bin)
+def test_closure(x, y):
+    def mul(a):
+        return a * x
+    return mul(x) + mul(y)
+
+
+@infer(
+    type=[
+        (i64, i64, i64, i64, T(i64, i64)),
+        (f64, f64, f64, f64, T(f64, f64)),
+        (i64, i64, f64, f64, T(i64, f64)),
+        (i64, f64, f64, f64, TypeError),
+        (i64, i64, i64, f64, TypeError),
+    ]
+)
+def test_return_closure(w, x, y, z):
+    def mul(a):
+        def clos(b):
+            return a * b
+        return clos
+    return (mul(w)(x), mul(y)(z))
+
+
+@infer(type=[(i64, i64), (f64, TypeError)])
+def test_fact(n):
+    def fact(n):
+        if n <= 1:
+            return 1
+        else:
+            return n * fact(n - 1)
+    return fact(n)
+
+
+def even(n):
+    if n == 0:
+        return True
+    else:
+        return odd(n - 1)
+
+
+def odd(n):
+    if n == 0:
+        return False
+    else:
+        return even(n - 1)
+
+
+@infer(type=[(i64, B), (f64, TypeError)])
+def test_even_odd(n):
+    return even(n)
+
+
+@infer(type=[(i64, i64), (f64, f64)])
+def test_pow10(x):
+    v = x
+    j = 0
+    while j < 3:
+        i = 0
+        while i < 3:
+            v = v * x
+            i = i + 1
+        j = j + 1
+    return v
+
+
+@infer(
+    type=[
+        (i64, i64, i64, i64),
+        (i64, f64, f64, f64)
+    ]
+)
+def test_choose_prim(i, x, y):
+    add, mul
+
+    def choose(i):
+        if i == 0:
+            return add
+        else:
+            return mul
+
+    return choose(i)(x, y)
+
+
+@infer(
+    type=[
+        (i64, i64, i64, TypeError),
+        ({'value': 0}, i64, i64, i64),
+        ({'value': 1}, i64, i64, B),
+    ]
+)
+def test_choose_prim_incompatible(i, x, y):
+    add, lt
+
+    def choose(i):
+        if i == 0:
+            return add
+        else:
+            return lt
+
+    return choose(i)(x, y)
+
+
+@infer(
+    type=[
+        (i64, i64, i64, TypeError),
+        ({'value': 0}, i64, i64, i64),
+        ({'value': 1}, i64, i64, B),
+    ]
+)
+def test_choose_incompatible(i, x, y):
+
+    def add2(x, y):
+        return x + y
+
+    def lt2(x, y):
+        return x < y
+
+    def choose(i):
+        if i == 0:
+            return add2
+        else:
+            return lt2
+
+    return choose(i)(x, y)
+
+
+@infer(
+    type=[
+        (i64, i64, i64),
+        (i64, f64, f64)
+    ]
+)
+def test_choose_indirect(i, x):
+
+    def double(x):
+        return x + x
+
+    def square(x):
+        return x * x
+
+    def choose(i):
+        if i == 0:
+            return double
+        else:
+            return square
+
+    return choose(i)(x)
+
+
+@infer(
+    type=[
+        (i64, i64)
+    ]
+)
+def test_hof(x):
+
+    def double(x):
+        return x + x
+
+    def square(x):
+        return x * x
+
+    def hof(f, tup):
+        return f(tup[0]) + f(tup[1])
+
+    return hof(double, (x + 1, x + 2)) + hof(square, (x + 3, x + 4))
+
+
+@infer(
+    type=[
+        (i64, T(T(i64, i64), T(B, B)))
+    ]
+)
+def test_hof_2(x):
+
+    def double(x):
+        return x + x
+
+    def is_zero(x):
+        return x == 0
+
+    def hof(f, tup):
+        return (f(tup[0]), f(tup[1]))
+
+    return (hof(double, (x + 1, x + 2)), hof(is_zero, (x + 3, x + 4)))
+
+
+@infer(
+    type=[
+        (i64, i64, TypeError),
+        ({'value': -1}, i64, i64),
+        ({'value': 1}, i64, T(i64, i64)),
+    ]
+)
+def test_hof_3(x, y):
+
+    def double(x):
+        return x + x
+
+    def hof_sum(f, tup):
+        return f(tup[0]) + f(tup[1])
+
+    def hof_tup(f, tup):
+        return (f(tup[0]), f(tup[1]))
+
+    def pick(x):
+        if x < 0:
+            return hof_sum
+        else:
+            return hof_tup
+
+    f = pick(x)
+    return f(double, (y + 3, y + 4))
+
+
+@infer(
+    type=[
+        (i64, i64, i64)
+    ]
+)
+def test_func_arg(x, y):
+     def g(func, x, y):
+         return func(x, y)
+
+     def h(x, y):
+         return x + y
+     return g(h, x, y)
+
+
+@infer(
+    type=[
+        (i64, TypeError)
+    ]
+)
+def test_func_arg3(x):
+    def g(func, x):
+        z = func + x
+        return func(z)
+
+    def h(x):
+        return x
+
+    return g(h, x)
+
+
+@infer(
+    type=[
+        (i64, i64),
+        (f64, f64),
+    ]
+)
+def test_func_arg4(x):
+    def h(x):
+        return x
+
+    def g(fn, x):
+        return fn(h, x)
+
+    def t(fn, x):
+        return fn(x)
+
+    return g(t, x)
+
+
+@infer(
+    type=[
+        ({'value': 4},),
+    ]
+)
+def test_closure_deep():
+    def g(x):
+        def h():
+            return x * x
+        return h
+    return g(2)()
+
+
+@infer(
+    type=[
+        (i64, i64, i64)
+    ],
+    value=[
+        (5, 7, 15)
+    ]
+)
+def test_closure_passing(x, y):
+    def adder(x):
+        def f(y):
+            return x + y
+        return f
+
+    a1 = adder(1)
+    a2 = adder(2)
+
+    return a1(x) + a2(y)
