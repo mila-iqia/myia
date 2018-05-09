b427785d6996d2274f1e9e0c756038c0d31b32f8
abergeron <bergearn@iro.umontreal.ca>
2018-05-09 15:07:59 -0400

    Better (and simpler) debug vm implementation. (#63)

--- myia/vm.py
+++ myia/vm.py
@@ -5,328 +5,212 @@ testing or debugging.  Don't expect stellar performance from this
 implementation.
 """
 from types import FunctionType
-from typing import Any, Callable, Dict, List, Set, Union
+from typing import Iterable, Mapping, Any, Callable, List
 
-from .ir import ANFNode, Apply, Constant, Graph, Parameter, dfs
-from .prim import Primitive, ops as primops
+from .ir import Graph, Apply, Constant, Parameter, ANFNode
+from .ir.utils import is_constant_graph
+from .prim import Primitive
+from .prim.ops import if_, return_
 from .utils import smap
+from .graph_utils import toposort
+from .cconv import NestingAnalyzer
 
 
-class CallableClosure:
-    """Callable closure."""
+class VMFrame:
+    """An execution frame.
 
-    def __init__(self, closure):
-        """Initialize the CallableClosure."""
+    This holds the state for an application of a graph.  The todo list
+    must contain free variables of graphs encountered before the
+    graph themselves.
+
+    You can index a frame with a node to get its value in the context
+    of this frame (if it has already been evaluated).
+
+    Attributes:
+        values: Mapping of node to their values in this application
+        todo: list of nodes remaining to execute
+        closure: values for the closure if the current application is a closure
+
+    """
+
+    def __init__(self, nodes: Iterable[ANFNode], values: Mapping[ANFNode, Any],
+                 *, closure: Mapping[ANFNode, Any] = None) -> None:
+        """Initialize a frame."""
+        self.values = dict(values)
+        self.todo = list(nodes)
+        self.todo.reverse()
         self.closure = closure
 
-    def __call__(self, *args):
-        """Call the CallableClosure."""
-        vm = self.closure.frame.vm
-        return vm.evaluate(self.closure.graph, args, self.closure.frame)
+    def __getitem__(self, node: ANFNode):
+        if node in self.values:
+            return self.values[node]
+        elif self.closure is not None and node in self.closure:
+            return self.closure[node]
+        else:
+            # Should be a constant
+            return node.value
+
+
+class Closure:
+    """Representation of a closure."""
+
+    def __init__(self, graph: Graph, values: Mapping[ANFNode, Any]) -> None:
+        """Build a closure."""
+        self.graph = graph
+        self.values = values
+        self.vm: 'VM' = None
+
+    def __call__(self, *args, vm: 'VM' = None):
+        """Evaluates the closure."""
+        if vm is None:
+            vm = self.vm
+        return vm.evaluate(self.graph, args, closure=self.values)
 
 
 class VM:
     """Virtual Machine interface."""
 
-    def __init__(self, implementations: Dict[Primitive, Callable]) -> None:
+    class _Call(Exception):
+        """Indicate a call to a new frame."""
+
+        def __init__(self, frame):
+            self.frame = frame
+
+    class _Return(Exception):
+        """Indicates a return with its value."""
+
+        def __init__(self, value):
+            self.value = value
+
+    def __init__(self, implementations: Mapping[Primitive, Callable]) \
+            -> None:
         """Initialize the VM."""
         self.implementations = implementations
-
-    def make_callable(self, g):
-        """Return an object that executes `g` when called on arguments."""
-        return CallableClosure(VMFrame.Closure(g, VMFrame(self, g, [], None)))
 
     def convert_value(self, value):
         """Translate the value to a format that the VM understands."""
         if isinstance(value, FunctionType):
             from .api import parse
             return parse(value)
-        elif isinstance(value, CallableClosure):
-            return value.closure
         else:
             return value
 
     def unconvert_value(self, value):
         """Translate a VM-produced value to a user-faced format."""
-        if isinstance(value, VMFrame.Closure):
-            return CallableClosure(value)
-        elif isinstance(value, Primitive):
+        if isinstance(value, Primitive):
             return self.implementations[value]
+        elif isinstance(value, Closure):
+            value.vm = self
+            return value
+        elif isinstance(value, Graph):
+            return self.make_callable(value)
         else:
             return value
 
-    def evaluate(self,
-                 graph: Graph,
-                 args: List[Any],
-                 closure: 'VMFrame' = None) -> Any:
+    def make_callable(self, g):
+        """Return an object that executes `g` when called on arguments."""
+        c = Closure(g, None)
+        c.vm = self
+        return c
+
+    def evaluate(self, graph: Graph, _args: Iterable[Any], *,
+                 closure: Mapping[ANFNode, Any] = None) -> Any:
         """Run a graph.
 
         This will evaluate the passed-in graph and return the
         resulting value.
         """
-        args = smap(self.convert_value, args)
-        root_frame = VMFrame(self, graph, args, None, closure)
-        frame = root_frame
-        while frame is not None:
-            frame = frame.run()
-        return smap(self.unconvert_value, root_frame.result())
+        args = smap(self.convert_value, tuple(_args))
 
+        N = NestingAnalyzer(graph)
+        self._vars = N.free_variables_total()
 
-class VMFrame:
-    """
-    An execution frame.
+        if len(args) != len(graph.parameters):
+            raise RuntimeError("Call with wrong number of arguments")
 
-    This is the main class for the VM.  It does all the bookkeeping to
-    handle the execution state.
+        top_frame = VMFrame(toposort(graph.return_, self._succ_vm),
+                            dict(zip(graph.parameters, args)),
+                            closure=closure)
+        frames = [top_frame]
 
-    Attributes:
-        graph: The currently executing graph.  This can change due to
-            a tail call.
-        args: Argument array for the current call.
-        parent: VMFrame that we return to.
-        closure: VMFrame to look up free variables in.
-        values: Mapping of every node in the graph to its computed
-            value.  Currently all the values are always kept live.
-        todo: List of node that have to be visited.  Evaluation starts
-            from the end of the list.
-
-    """
-
-    class Continue(Exception):
-        """Continue step."""
-
-    class Return(Exception):
-        """End step."""
-
-        def __init__(self, value: 'VMFrame' = None) -> None:
-            """Which value to return."""
-            super().__init__()
-            self.value: VMFrame = value
-
-    class Closure:
-        """Representation of a closure."""
-
-        def __init__(self, graph: Graph, frame: 'VMFrame') -> None:
-            """Build a closure."""
-            self.graph = graph
-            self.frame = frame
-
-    class Jump(ANFNode):
-        """Jump Node for out of scope values."""
-
-        def __init__(self, frame: 'VMFrame') -> None:
-            """Set a jump."""
-            super().__init__([], frame, None)
-
-    def __init__(self,
-                 vm: VM,
-                 graph: Graph,
-                 args: List[Any],
-                 parent: 'VMFrame',
-                 closure: 'VMFrame' = None) -> None:
-        """Build a frame."""
-        self.vm = vm
-        self.graph = graph
-        self.args = args
-        self.parent = parent
-        self.closure = closure
-        self.values: Dict[ANFNode, Any] = dict()
-        self.todo: List[ANFNode] = [self.graph.return_]
-
-    def run(self) -> 'VMFrame':
-        """Run the graph to completion and return the result.
-
-        Returns:
-            The next frame to execute. Can be either the frame of a (tail)
-            call, the frame of the parent (if the current frame completed), or
-            `None` (if the program terminated).
-
-        """
-        while True:
+        while frames:
             try:
-                res = self.advance()
-            except StopIteration:
-                # Return is handled here because the flow control is a bit
-                # too complex for the parent frame to get a value back
-                # from the called frame.
-                if self.parent is not None:
-                    self.parent.values[self.parent.todo[-1]] = self.result()
-                    self.parent.todo.pop()
-                return self.parent
-            if res is not None:
-                return res
-
-    def tail(self) -> bool:
-        """Determine whether the current call is a tail call."""
-        return self.todo[-2] is self.graph.return_
-
-    def done(self) -> bool:
-        """Determine whether this function has fully evaluated."""
-        return not self.todo
-
-    def result(self):
-        """Get this frame's result."""
-        return self.values[self.graph.return_]
-
-    def get_value(self, node: ANFNode):
-        """Get the value for a node.
-
-        If the value for a node is not available, the node will be added to the
-        todo list. Evaluation of the current node in `eval_node` will be halted
-        (by raising `Continue`) and control will be returned to the `advance`
-        method in order to evaluate the last item on the todo list.
-
-        If the value is not available and the node belongs another graph, we
-        will evaluate that node in a different frame (by raising `Return`). A
-        `Jump` instruction will be added to that frame to jump back to the
-        current one.
-
-        Returns:
-            The value of the node.
-
-        Raises:
-            Continue: If the node's value is not available and its calculation
-                has been added to the todo list.
-            Return: If the node's value is not available and must be retreived
-                from an enclosing frame, or from a closure.
-
-        """
-        frame = self
-        while frame and node.graph and node.graph is not frame.graph:
-            frame = frame.closure
-        if node not in frame.values:
-            # we set up a fake node to jump back to this frame after
-            # the value that we need has been evaluated.
-            if frame is not self:
-                frame.todo.append(self.Jump(self))
-            frame.todo.append(node)
-            if frame is not self:
-                raise self.Return(frame)
-            else:
-                raise self.Continue
-        return frame.values[node]
-
-    def wrap_closure(self, value):
-        """Wrap graphs that are closures."""
-        if isinstance(value, Graph):
-            subgraphs: Set[Graph] = set([value])
-            seen: Set[Graph] = set()
-            targets: Set[Graph] = set()
-            while len(subgraphs) != 0:
-                curg = subgraphs.pop()
-                if curg in seen:
-                    continue
-                seen.add(curg)
-                for n in dfs(curg.return_):
-                    if n.graph and n.graph is not curg and n.graph not in seen:
-                        targets.add(n.graph)
-                    if (isinstance(n, Constant) and
-                            isinstance(n.value, Graph)):
-                        subgraphs.add(n.value)
-            if len(targets) > 0:
-                frame = self
-                while frame and frame.graph not in targets:
-                    frame = frame.parent
-                if frame is None:
-                    frame = self.closure
-                while frame and frame.graph not in targets:
-                    frame = frame.parent  # pragma: no cover
-                assert frame is not None
-                return self.Closure(value, frame)
-        return value
-
-    def do_call(self, graph: Union[Graph, 'VMFrame.Closure'], args: List[Any]):
-        """Perform a call.
-
-        This will handle tail calls and closures.
-
-        Raises:
-            Return: Giving control to the new frame in which the function
-                (graph) will be evaluated.
-
-        """
-        if self.tail() and self.parent is not None:
-            parent = self.parent
-        else:
-            parent = self
-        if isinstance(graph, self.Closure):
-            new_frame = VMFrame(self.vm, graph.graph, args, parent,
-                                graph.frame)
-        else:
-            new_frame = VMFrame(self.vm, graph, args, parent)
-        assert len(new_frame.graph.parameters) == len(new_frame.args)
-        raise self.Return(new_frame)
-
-    def advance(self) -> 'VMFrame':
-        """Take a step.
-
-        This will perform one "step" of execution.  This corresponds roughly to
-        evaluating one node of the graph. In the case of function calls, we
-        single-step through the called function even though the apply is a
-        single node in our graph.
-
-        Returns:
-            The next frame to execute. Can be either a new function call, a
-            tail call, or a parent frame in the case of closures/accessing the
-            enclosed scope. Returns `None` when the program terminated.
-
-        Raises:
-            StopIteration: If there are no more steps to take and control
-                should be returned to the `run` function.
-
-        """
-        if self.done():
-            raise StopIteration
-
-        while True:
-            node = self.todo[-1]
-            assert node not in self.values
-
-            try:
-                self.eval_node(node)
-            except self.Continue:
-                continue
-            except self.Return as e:
-                return e.value
-
-            self.todo.pop()
-            return None
-
-    def eval_node(self, node: ANFNode):
-        """Compute the value for a node.
-
-        This may fail and ask for a missing value to be computed or enter into
-        a call.
-
-        Raises:
-            Return: If the node is a jump instruction (see `get_value`),
-                returning control to another frame.
-            ValueError: If the node is the application of an unknown primitive.
-
-        """
-        if isinstance(node, Constant):
-            self.values[node] = self.wrap_closure(node.value)
-        elif isinstance(node, Parameter):
-            idx = self.graph.parameters.index(node)
-            self.values[node] = self.args[idx]
-        elif isinstance(node, self.Jump):
-            # Normally `advance` pops the last node after `eval_node`
-            # completes, but here we do it ourselves because control will be
-            # returned to `run` directly
-            self.todo.pop()
-            raise self.Return(node.value)
-        elif isinstance(node, Apply):
-            fn = self.get_value(node.inputs[0])
-            args = [self.get_value(i) for i in node.inputs[1:]]
-            if isinstance(fn, Primitive):
-                if fn is primops.if_:
-                    cond, tb, fb = args
-                    self.do_call(tb if cond else fb, [])
-                elif fn in self.vm.implementations:
-                    impl = self.vm.implementations[fn]
-                    self.values[node] = impl(*args)
+                frame = frames[-1]
+                todo = frame.todo
+                while todo:
+                    self._handle_node(todo[-1], frame)
+                    todo.pop()
+            except self._Call as c:
+                # The last element of todo is always a return
+                if len(todo) == 2:
+                    frames[-1] = c.frame
                 else:
-                    raise ValueError(f'Unknown primitive: {fn}') \
-                        # pragma: no cover
+                    frames.append(c.frame)
+            except self._Return as r:
+                frames.pop()
+                if frames:
+                    frames[-1].values[frames[-1].todo[-1]] = r.value
+                    frames[-1].todo.pop()
+                else:
+                    return smap(self.unconvert_value, r.value)
+
+    def _succ_vm(self, node: ANFNode) -> Iterable[ANFNode]:
+        """Follow node.incoming and free variables."""
+        for i in node.inputs:
+            if i.graph == node.graph or is_constant_graph(i):
+                yield i
+        if is_constant_graph(node):
+            yield from self._vars[node.value]
+
+    def _call(self, graph: Graph, args: List[Any]):
+        clos = None
+        if isinstance(graph, Closure):
+            clos = graph.values
+            graph = graph.graph
+
+        if len(args) != len(graph.parameters):
+            raise RuntimeError("Call with wrong number of arguments")
+
+        raise self._Call(VMFrame(toposort(graph.return_, self._succ_vm),
+                                 dict(zip(graph.parameters, args)),
+                                 closure=clos))
+
+    def _make_closure(self, graph: Graph, frame: VMFrame) -> Closure:
+        clos = dict()
+        for v in self._vars[graph]:
+            clos[v] = frame[v]
+        return Closure(graph, clos)
+
+    def _handle_node(self, node: ANFNode, frame: VMFrame):
+        if isinstance(node, Constant):
+            if frame.closure is not None and node in frame.closure:
+                return
+
+            # We only visit constant graphs
+            assert is_constant_graph(node)
+            g = node.value
+            if len(self._vars[g]) != 0:
+                frame.values[node] = self._make_closure(g, frame)
+            # We don't need to do anything special for non-closures
+
+        elif isinstance(node, Parameter):
+            pass
+
+        elif isinstance(node, Apply):
+            fn = frame[node.inputs[0]]
+            args = [frame[a] for a in node.inputs[1:]]
+            if isinstance(fn, Primitive):
+                if fn == if_:
+                    if args[0]:
+                        self._call(args[1], [])
+                    else:
+                        self._call(args[2], [])
+                elif fn == return_:
+                    raise self._Return(args[0])
+                else:
+                    frame.values[node] = self.implementations[fn](*args)
             else:
-                self.do_call(fn, args)
+                self._call(fn, args)
+
+        else:
+            raise AssertionError("Unknown node type")  # pragma: no cover
