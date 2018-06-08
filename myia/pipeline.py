4a44e3b7bfd272d323d2573d33da7dc683d682fd
Olivier Breuleux <breuleux@gmail.com>
2018-06-08 14:49:46 -0400

    Pipeline

---
+++ myia/pipeline.py
@@ -0,0 +1,511 @@
+"""Tools to generate and configure Myia's operation pipeline."""
+
+
+import inspect
+from myia.utils import TypeMap
+from myia.utils import Named
+
+
+# Use in a merge to indicate that a key should be deleted
+DELETE = Named('DELETE')
+
+
+# Placeholder for MergeMode.__init__
+_ABSENT = Named('_ABSENT')
+
+
+class MergeMode:
+    """Wraps a value to control how it is merged.
+
+    Class attributes:
+        mode: The merge mode to use.
+
+    Attributes:
+        value: The value to merge.
+
+    """
+
+    mode = 'merge'  # NOTE: This is the default merge mode used by merge()
+
+    def __init__(self, __value=_ABSENT, **kwargs):
+        """Initialize a MergeMode.
+
+        The value to merge will be the single argument given, or if there is no
+        positional argument, the keywords dictionary.
+        """
+        if __value is _ABSENT:
+            self.value = kwargs
+        else:
+            assert not kwargs
+            self.value = __value
+
+
+class Merge(MergeMode):
+    """Merge normally."""
+
+    mode = 'merge'
+
+
+class Override(MergeMode):
+    """Do not concatenate sequences."""
+
+    mode = 'override'
+
+
+class Reset(MergeMode):
+    """Throw away the previous value."""
+
+    mode = 'reset'
+
+
+_cleanup_map = TypeMap()
+
+
+@_cleanup_map.register(object)
+def _cleanup_object(value):
+    return value
+
+
+@_cleanup_map.register(MergeMode)
+def _cleanup_MergeMode(mm):
+    return mm.value
+
+
+@_cleanup_map.register(dict)
+def _cleanup_dict(d):
+    return type(d)({k: cleanup(v) for k, v in d.items() if v is not DELETE})
+
+
+def _cleanup_sequence(xs):
+    return type(xs)(cleanup(x) for x in xs)
+
+
+_cleanup_map.register(tuple, _cleanup_sequence)
+_cleanup_map.register(list, _cleanup_sequence)
+_cleanup_map.register(set, _cleanup_sequence)
+
+
+def cleanup(x):
+    """Remove all MergeMode and DELETE instances from the data."""
+    return _cleanup_map[type(x)](x)
+
+
+_merge_map = TypeMap(discover=lambda cls: getattr(cls, '__merge__', None))
+
+
+@_merge_map.register(dict)
+def _merge_dict(d1, d2, mode):
+    if mode == 'reset':
+        return type(d1)(d2)
+
+    rval = type(d1)()
+    for k, v in d1.items():
+        if k in d2:
+            v2 = d2[k]
+            if v2 is DELETE:
+                pass
+            else:
+                rval[k] = merge(v, v2, mode)
+        else:
+            rval[k] = v
+    for k, v in d2.items():
+        if k not in d1:
+            rval[k] = cleanup(v)
+    return rval
+
+
+@_merge_map.register(tuple)
+def _merge_tuple(xs, ys, mode):
+    xs = cleanup(xs)
+    ys = cleanup(ys)
+    if mode == 'merge':
+        return xs + ys
+    else:
+        return ys
+
+
+@_merge_map.register(list)
+def _merge_list(xs, ys, mode):
+    xs = cleanup(xs)
+    ys = cleanup(ys)
+    if mode == 'merge':
+        return xs + ys
+    else:
+        return ys
+
+
+@_merge_map.register(set)
+def _merge_set(xs, ys, mode):
+    xs = cleanup(xs)
+    ys = cleanup(ys)
+    if mode == 'merge':
+        return xs | ys
+    else:
+        return ys
+
+
+@_merge_map.register(object)
+def _merge_object(a, b, mode):
+    return cleanup(b)
+
+
+def merge(a, b, mode=MergeMode.mode):
+    """Merge two data structures.
+
+    Arguments:
+        a: The original data.
+        b: The data to merge.
+        mode:
+            'merge': (default) Sequences will be concatenated, sets merged,
+                and dictionaries merged according to their keys.
+            'override': Dictionaries are merged, but sequences are not
+                concatenated.
+            'reset': b is returned, or takes primacy over the data in a.
+    """
+    if isinstance(b, MergeMode):
+        mode = b.mode
+        b = b.value
+    assert not isinstance(a, MergeMode)
+    return _merge_map[type(a)](a, b, mode)
+
+
+class NS:
+    """Namespace for pipeline stages.
+
+    This namespace preserves key order in the representation, unlike
+    types.SimpleNamespace.
+    """
+
+    def __init__(self, **kwargs):
+        """Initialize NS."""
+        self.__dict__.update(kwargs)
+
+    # def __getitem__(self, item):
+    #     return self.__dict__[item]
+
+    def __setitem__(self, item, value):
+        self.__dict__[item] = value
+
+    def __repr__(self):
+        args = [f'{k}={v}' for k, v in self.__dict__.items()]
+        return f'NS({", ".join(args)})'
+
+
+class Partial:
+    """Partial application of a function.
+
+    This differs from functools.partial in a few ways:
+
+    * Only keyword arguments are accepted.
+    * Argument names are validated immediately.
+    * It is possible to merge two partials, with the second updating the
+      parameters of the first. It is also possible to merge a dict and
+      a Partial. Merge and Override work basically like on dictionaries.
+    * `merge(partial1, Override(partial2))` lets us change the
+      constructor.
+    """
+
+    def __init__(self, func, *,
+                 _wrap=False,
+                 **keywords):
+        """Initialize a Partial."""
+        self.wrap = _wrap
+        self.func = func
+        self.keywords = keywords
+        self._validate()
+
+    def _validate(self):
+        """Check that all the argument names are valid."""
+        if isinstance(self.func, type):
+            f = getattr(self.func, '__init__', self.func)
+        else:
+            f = self.func
+        spec = inspect.getfullargspec(f)
+        if spec.varkw:
+            return
+        valid = spec.args + spec.kwonlyargs
+        for k, v in self.keywords.items():
+            if k not in valid:
+                raise TypeError(f"{f} has no argument named '{k}'")
+
+    def __call__(self, **kwargs):
+        """Run the pipeline on the given arguments, from start to finish."""
+        value = self.func(**self.keywords, **kwargs)
+        if self.wrap:
+            value = self.wrap(value)
+        return value
+
+    def __merge__(self, partial, mode):
+        """Combine arguments from two partials."""
+        if isinstance(partial, dict):
+            partial = Partial(self.func, **partial)
+
+        assert isinstance(partial, Partial)
+
+        if partial.func is self.func \
+                or mode == 'override' or mode == 'reset':
+            func = partial.func
+        else:
+            raise ValueError('Incompatible func')
+
+        if mode == 'reset':
+            kwargs = partial.keywords
+        elif mode == 'override':
+            kwargs = {**self.keywords, **partial.keywords}
+        else:
+            kwargs = merge(self.keywords, partial.keywords)
+
+        wrap = partial.wrap or self.wrap
+        return Partial(func, _wrap=wrap, **kwargs)
+
+    def __repr__(self):
+        args = [f'{k}={v}' for k, v in self.keywords.items()]
+        return f'{self.func.__name__}({", ".join(args)})'
+
+
+class Partializable:
+    """Class for which partial instances may be created.
+
+    This defines `Class.partial(arg=val, ...)`, which is equivalent to
+    `Partial(Class, arg=val, ...)`.
+    """
+
+    @classmethod
+    def partial(cls, **kwargs):
+        """Return a Partial on this class constructor."""
+        return Partial(cls, **kwargs)
+
+
+class PipelineDefinition:
+    """Defines a Pipeline.
+
+    A PipelineDefinition can be configured in a fine-grained manner.
+    Call `make()` to instantiate a Pipeline that can be executed.
+
+    Attributes:
+        resources: An ensemble of common resources, pooled by the whole
+            pipeline.
+        steps: A sequence of steps. Each step should be a Partial on a
+            PipelineStep.
+
+    """
+
+    def __init__(self, *, resources=None, steps):
+        """Initialize a PipelineDefinition."""
+        self.resources = resources or {}
+        self.steps = list(steps.items())
+        self.step_names = list(steps.keys())
+
+    def index(self, step, upper_bound=False):
+        """Return the index corresponding to the given step.
+
+        Args:
+            step: The name of the step, or an int (returned without
+                modification).
+            upper_bound: Whether this is meant to be used as an
+                upper bound or not. If it is True, then:
+                * 1 will be added to the result if the step is a
+                  string, so that it becomes inclusive,
+                * unless it starts with !
+                * Integers are still returned without modification.
+        """
+        if isinstance(step, str):
+            if step.startswith('!'):
+                return self.step_names.index(step[1:])
+            else:
+                plus = 1 if upper_bound else 0
+                return self.step_names.index(step) + plus
+        elif isinstance(step, int) or step is None:
+            return step
+        else:  # pragma: no cover
+            raise TypeError('index should be a string or int')
+
+    def getslice(self, item):
+        """Transform an item or a string slice into a numeric slice.
+
+        The index for a string corresponds to a step name.
+
+        >>> pd = PipelineDefinition(steps=dict(a=..., b=..., c=...))
+        >>> pd.getslice("b")
+        slice(1, 2)
+        >>> pd.getslice(slice("b", "c"))
+        slice(1, 3)
+        """
+        if isinstance(item, slice):
+            assert item.step is None
+            return slice(self.index(item.start),
+                         self.index(item.stop, True))
+        else:
+            return slice(0, self.index(item, True))
+
+    def insert_before(self, step=0, **new_steps):
+        """Insert new steps before the given step.
+
+        If no step is given, the new steps are inserted at the very beginning.
+
+        This returns a new PipelineDefinition.
+        """
+        steps = list(self.steps)
+        index = self.index(step) or 0
+        steps[index:index] = new_steps.items()
+        return PipelineDefinition(
+            resources=self.resources,
+            steps=dict(steps)
+        )
+
+    def insert_after(self, step=None, **new_steps):
+        """Insert new steps after the given step.
+
+        If no step is given, the new steps are inserted at the very end.
+
+        This returns a new PipelineDefinition.
+        """
+        steps = list(self.steps)
+        if step is None:
+            steps.extend(new_steps.items())
+        else:
+            index = self.index(step)
+            steps[index + 1:index + 1] = new_steps.items()
+        return PipelineDefinition(
+            resources=self.resources,
+            steps=dict(steps)
+        )
+
+    def configure_resources(self, **resources):
+        """Change resources or define new resources.
+
+        This returns a new PipelineDefinition.
+        """
+        return PipelineDefinition(
+            resources=merge(self.resources, resources),
+            steps=dict(self.steps)
+        )
+
+    def configure(self, changes={}, **kwchanges):
+        """Configure existing resources and steps.
+
+        To add new steps, use `insert_before` or `insert_after`. To add new
+        resources, use `configure_resources`.
+
+        This returns a new PipelineDefinition.
+
+        By default, new data is merged. These are all equivalent:
+
+        >>> pd.configure(mystep={'param': 234})
+        >>> pd.configure(mystep=Merge(param=234))
+        >>> pd.configure({'mystep.param': 234})
+
+        `mystep` will keep all of its previous parameters, except for the
+        overriden one. If the parameter is a list, the list given in the
+        update will be appended to the previous, but you can use
+        `Override([a, b, c])` to replace the old list.
+        """
+        def activator(active):
+            def wrap(x):
+                x.active = active
+                return x
+            return wrap
+        new_data = {**dict(self.steps), **self.resources}
+        changes = {**changes, **kwchanges}
+        for path, delta in changes.items():
+            if isinstance(delta, bool) and path in self.step_names:
+                delta = dict(_wrap=activator(delta))
+            top, *parts = path.split('.')
+            for part in reversed(parts):
+                delta = {part: delta}
+            new_data[top] = merge(new_data[top], delta)
+        return PipelineDefinition(
+            resources={k: new_data[k] for k in self.resources},
+            steps={k: new_data[k] for k in self.step_names}
+        )
+
+    def make(self):
+        """Create a Pipeline from this definition."""
+        return Pipeline(self)
+
+    def __getitem__(self, item):
+        """Return a pipeline that only contains a subset of the steps."""
+        return PipelineDefinition(
+            resources=self.resources,
+            steps=dict(self.steps[self.getslice(item)])
+        )
+
+
+class Pipeline:
+    """Sequence of connected processing steps.
+
+    Created from a PipelineDefinition. Each step processes the output of the
+    previous step. A Pipeline also defines common resources for all the steps.
+    """
+
+    def __init__(self, defn):
+        """Initialize a Pipeline from a PipelineDefinition."""
+        def convert(x, name):
+            if isinstance(x, Partial):
+                x = x()
+            if hasattr(x, 'set_pipeline'):
+                x.set_pipeline(pipeline=self, name=name)
+            return x
+
+        self.defn = defn
+        self.resources = NS()
+        self.steps = NS()
+        self._seq = []
+        for k, v in defn.resources.items():
+            self.resources[k] = convert(v, None)
+        for k, v in defn.steps:
+            v = convert(v, k)
+            self.steps[k] = v
+            self._seq.append(v)
+
+    def __getitem__(self, item):
+        """Return a sub-pipeline.
+
+        item may be the name of a step, in which case the resulting
+        sub-pipeline goes from the beginning to that step (inclusive), or it
+        may be a range that starts at a given step and ends at another
+        (again, inclusive).
+        """
+        return _PipelineSlice(self, item)
+
+    def __call__(self, **args):
+        """Execute the pipeline from start to finish."""
+        return self[:](**args)
+
+
+class _PipelineSlice:
+    def __init__(self, pipeline, slice):
+        self.pipeline = pipeline
+        self.slice = self.pipeline.defn.getslice(slice)
+
+    def __call__(self, **args):
+        for step in self.pipeline._seq[self.slice]:
+            if step.active:
+                args = step.step(**args)
+        return args
+
+
+class PipelineStep(Partializable):
+    """Step in a Pipeline."""
+
+    def __init__(self):
+        """Initialize a PipelineStep."""
+        self.active = True
+
+    def set_pipeline(self, pipeline, name):
+        """Set the pipeline field to this step's parent pipeline."""
+        self.pipeline = pipeline
+        self.name = name
+        self.steps = pipeline.steps
+        self.resources = pipeline.resources
+
+    def step(self, **kwargs):
+        """Execute this step only.
+
+        The received arguments come from the previous step.
+        """
+        raise NotImplementedError()
+
+    def __call__(self, **args):
+        """Execute the pipeline all the way up to this step."""
+        return self.pipeline[:self.name](**args)
---
+++ tests/test_pipeline.py
@@ -0,0 +1,268 @@
+
+import pytest
+from myia.pipeline import merge, Partial, PipelineStep, \
+    PipelineDefinition, Merge, Reset, Override, DELETE, cleanup
+from myia.utils import TypeMap
+
+
+class OpStep(PipelineStep):
+
+    def __init__(self, op, param=0):
+        super().__init__()
+        self.op = op
+        self.param = param
+
+    def step(self, value):
+        return {'value': self.op(self.param, value)}
+
+
+class OpResourceStep(PipelineStep):
+
+    def __init__(self, op):
+        super().__init__()
+        self.op = op
+
+    def step(self, value):
+        return {'value': self.op(self.resources.param, value)}
+
+
+@pytest.fixture
+def op_pipeline():
+    return PipelineDefinition(
+        resources=dict(
+            param=2
+        ),
+        steps=dict(
+            addp=OpStep.partial(op=lambda p, x: p + x, param=1),
+            mulp=OpResourceStep.partial(op=lambda p, x: p * x),
+            neg=OpStep.partial(op=lambda p, x: -x),
+            square=OpStep.partial(op=lambda p, x: x * x),
+        )
+    )
+
+
+def test_merge():
+
+    assert merge(1, 2) == 2
+    assert merge([1, 2], [3, 4]) == [1, 2, 3, 4]
+    assert merge((1, 2), (3, 4)) == (1, 2, 3, 4)
+    assert merge({1, 2}, {3, 4}) == {1, 2, 3, 4}
+
+    a = dict(a=1, b=2, c=dict(d=3, e=[4, 5]))
+    b = dict(a=1, b=2, c=dict(e=[6, 7], f=8))
+    c = dict(a=1, b=2, c=dict(d=3, e=[4, 5, 6, 7], f=8))
+
+    assert merge(a, b) == c
+
+    dlt = dict(c=DELETE, d=3)
+    assert merge(a, dlt) == dict(a=1, b=2, d=3)
+
+
+def test_merge_subclass():
+
+    tm = TypeMap({int: "int"})
+    mtm = merge(tm, {str: "str"})
+    assert isinstance(mtm, TypeMap)
+    assert mtm == TypeMap({int: "int", str: "str"})
+
+
+def test_merge_modes():
+
+    for x, y in [({1, 2}, {3, 4}),
+                 ([1, 2], [3, 4]),
+                 ((1, 2), (3, 4))]:
+
+        assert merge(x, y, mode='reset') == y
+        assert merge(x, Reset(y)) == y
+        assert merge(x, Reset(y), mode='merge') == y
+
+        assert merge(x, y, mode='override') == y
+        assert merge(x, Override(y)) == y
+        assert merge(x, Override(y), mode='merge') == y
+
+    a = {'a': 1}
+    b = {'b': 2}
+    c = {'a': 1, 'b': 2}
+
+    assert merge(a, b, mode='reset') == b
+    assert merge(a, b, mode='override') == c
+
+    a = {'a': [1, 2], 'b': [3, 4]}
+    b = {'a': [5, 6], 'b': Override([7, 8])}
+    c = {'a': [1, 2, 5, 6], 'b': [7, 8]}
+    d = {'a': [5, 6], 'b': [7, 8]}
+
+    assert merge(a, b) == c
+    assert merge(a, b, mode='override') == d
+
+
+def test_cleanup():
+    a = dict(a=1, b=[2, Merge(3)], c=Override(4), d=DELETE)
+    assert cleanup(a) == dict(a=1, b=[2, 3], c=4)
+
+
+def test_cleanup_subclass():
+    a = TypeMap({int: Merge("int")})
+    ca = cleanup(a)
+    assert isinstance(ca, TypeMap)
+    assert ca == TypeMap({int: "int"})
+
+
+def test_Partial():
+
+    def f(x, y):
+        return x + y
+
+    p1 = Partial(f, x=10)
+    p2 = Partial(f, y=20)
+
+    assert p1(y=3) == 13
+    assert p2(x=3) == 23
+    assert merge(p1, p2)() == 30
+    assert merge(p1, {'y': 20})() == 30
+
+    with pytest.raises(TypeError):
+        Partial(f, z=10)
+
+    p3 = Reset(Partial(f, y=20))
+    with pytest.raises(TypeError):
+        merge(p1, p3)()
+
+    p4 = Partial(f, x=[1, 2])
+    p5 = Partial(f, x=[3, 4])
+
+    assert merge(p4, p5)(y=[10]) == [1, 2, 3, 4, 10]
+    assert merge(p5, p4)(y=[10]) == [3, 4, 1, 2, 10]
+
+    p6 = Override(Partial(f, x=[3, 4]))
+    assert merge(p4, p6)(y=[10]) == [3, 4, 10]
+
+    def g(x, y):
+        return x * y
+
+    p7 = Partial(g, y=20)
+    with pytest.raises(ValueError):
+        merge(p1, p7)
+
+    p8 = Override(Partial(g, y=20))
+    assert merge(p1, p8)() == 200
+
+    def h(**kw):
+        return kw
+
+    p9 = Partial(h, x=10)
+    assert p9(y=20, z=30) == dict(x=10, y=20, z=30)
+
+
+def test_Partial_class():
+
+    class C:
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+
+        def __call__(self):
+            return self.x + self.y
+
+    p1 = Partial(C, x=10)
+    p2 = Partial(C, y=20)
+
+    assert p1(y=3)() == 13
+    assert p2(x=3)() == 23
+    assert merge(p1, p2)()() == 30
+
+    with pytest.raises(TypeError):
+        Partial(C, z=10)
+
+
+def test_Pipeline(op_pipeline):
+    pdef = op_pipeline
+
+    pip = pdef.make()
+    assert pip(value=3) == {'value': 64}
+
+    assert pip.steps.addp(value=3) == {'value': 4}
+    assert pip.steps.mulp(value=3) == {'value': 8}
+    assert pip.steps.neg(value=3) == {'value': -8}
+    assert pip.steps.square(value=3) == {'value': 64}
+
+    assert pip['addp'](value=3) == {'value': 4}
+    assert pip['mulp'](value=3) == {'value': 8}
+    assert pip['neg'](value=3) == {'value': -8}
+    assert pip['square'](value=3) == {'value': 64}
+
+    assert pip['mulp':](value=3) == {'value': 36}
+    assert pip[:'mulp'](value=3) == {'value': 8}
+    assert pip[:-1](value=3) == {'value': -8}
+    assert pip['mulp':'neg'](value=3) == {'value': -6}
+
+    assert pip['!square'](value=3) == {'value': -8}
+    assert pip['mulp':'!neg'](value=3) == {'value': 6}
+
+    assert pdef['mulp':'neg'].make()(value=3) == {'value': -6}
+
+
+def test_Pipeline_configure(op_pipeline):
+    pdef = op_pipeline
+
+    pip = pdef.configure(addp=Merge(param=2)).make()
+    assert pip(value=3) == {'value': 100}
+
+    pip = pdef.configure({'addp.param': 2}).make()
+    assert pip(value=3) == {'value': 100}
+
+    pip = pdef.configure(
+        {'addp.param': 2},
+        addp=Merge(op=lambda p, x: p - x)
+    ).make()
+    assert pip(value=3) == {'value': 4}
+
+    pip = pdef.configure(addp=Reset(op=lambda p, x: p - x, param=2)).make()
+    assert pip(value=3) == {'value': 4}
+
+    with pytest.raises(TypeError):
+        pdef.configure(addp=Reset(param=2)).make()
+
+    pip = pdef.configure(mulp=False).make()
+    assert pip(value=3) == {'value': 16}
+
+    pip = pdef.configure(mulp=False).configure(mulp=True).make()
+    assert pip(value=3) == {'value': 64}
+
+    pip = pdef.configure(mulp=False).configure(addp=False).make()
+    assert pip(value=3) == {'value': 9}
+
+    pip = pdef.configure(param=3).make()
+    assert pip(value=3) == {'value': 144}
+
+    with pytest.raises(KeyError):
+        pdef.configure(quack=[1, 2])
+
+    pdef2 = pdef.configure_resources(quack=[1, 2])
+    assert pdef2.make().resources.quack == [1, 2]
+    assert pdef2.configure(quack=[3]).make().resources.quack == [1, 2, 3]
+    assert pdef2.configure(quack=Reset([3])).make().resources.quack == [3]
+
+
+def test_Pipeline_insert(op_pipeline):
+    pdef = op_pipeline
+
+    half = OpStep.partial(op=lambda p, x: x / p, param=2)
+
+    pip = pdef.insert_before(half=half).make()
+    assert pip(value=3) == {'value': 25}
+
+    pip = pdef.insert_after(half=half).make()
+    assert pip(value=3) == {'value': 32}
+
+    pip = pdef.insert_before('addp', half=half).make()
+    assert pip(value=3) == {'value': 25}
+
+    pip = pdef.insert_after('addp', half=half).make()
+    assert pip(value=3) == {'value': 16}
+
+    pip = pdef.insert_before('square', half=half).make()
+    assert pip(value=3) == {'value': 16}
+
+    pip = pdef.insert_after('square', half=half).make()
+    assert pip(value=3) == {'value': 32}
