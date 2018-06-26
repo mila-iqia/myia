4c086becabdf8f9b5778ec9f93012051d4821931
Arnaud Bergeron <abergeron@gmail.com>
2018-06-26 15:22:56 -0400

    typo

--- myia/compile/utils.py
+++ myia/compile/utils.py
@@ -4,9 +4,9 @@ from ..ir import is_apply
 
 
 def get_outputs(lst, uses, seen):
-    """Return the list of node whose values are required beyond this segment.
+    """Return the list of nodes whose values are required beyond this segment.
 
-    Arguements:
+    Arguments:
         lst: list of nodes (the segment)
         uses: dict mapping each node to its uses (globally)
         seen: set of nodes that are part of the segment
