969f11869c40646a18035ab422894664c2132c52
Arnaud Bergeron <abergeron@gmail.com>
2018-11-29 17:41:12 -0500

    Add profiling support for pipelines

---
+++ myia/utils/profile.py
@@ -0,0 +1,29 @@
+def unit(secs):
+    return "%.3gs" % secs
+
+
+def print_profile(prof, *, indent=0):
+    steps = dict()
+    total = prof.pop('__total__')
+    count = prof.pop('__count__', None)
+    runtime = 0
+    ind = " " * indent
+
+    for v in prof.values():
+        if isinstance(v, dict):
+            v = v['__total__']
+        runtime += v
+
+    overhead = total - runtime
+
+    print(f"{ind}Total time taken: {unit(total)}")
+    if count is not None:
+        print(f"{ind}  Number of loops: {count}") 
+    print(f"{ind}  Overhead: {unit(overhead)}")
+    print(f"{ind}  Time spent in runtime: {unit(runtime)}")
+    for k, v in prof.items():
+        if isinstance(v, dict):
+            print(f"{ind}  {k}:")
+            print_profile(v, indent=indent + 4)
+        else:
+            print(f"{ind}  {k}: {unit(v)}")
