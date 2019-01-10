3d62b47c05a5011f19960c3a6c6c077f9097c9cd
Friedrich Yan <friedrichyan@Friedrichs-MacBook-Pro-2.local>
2019-01-10 09:57:17 +0800

    delete redundant vgg1.py

--- baseline/cifar10/models/tf/vgg.py
+++ baseline/cifar10/models/tf/vgg.py
@@ -9,8 +9,8 @@ class MiniVGG:
     """
     A mini version of VGG.
     
-    With hyper-parameters (lr=003, batch_size=128, dropout=0.1), the performance 
-    is test accuracy: 0.681600, test loss: 1.024199.
+    With hyper-parameters (lr=003, batch_size=128, dropout=0.1, epochs=10), the 
+    performance is test accuracy: 0.681600, test loss: 1.024199.
     """
     def __init__(self, lr=.003, dropout=0.5, name='mini_vgg'):
         self.sess = tf.Session()
