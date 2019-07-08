e9d7f90bea518aba1ec6c0b0581b38577d5fae69
abergeron <abergeron@gmail.com>
2019-07-08 16:38:20 -0400

    Unbreak relay backend following API change. (#226)

--- myia/compile/backends/relay_helpers.py
+++ myia/compile/backends/relay_helpers.py
@@ -4,48 +4,8 @@ Most of those should go away as Relay main development progresses.
 """
 
 from tvm import relay
-from tvm.relay import ir_pass
+from tvm.relay import transform
 import numpy as np
-
-
-class _LiveSet(relay.expr_functor.ExprMutator):
-    """Compute the live set of globals.
-
-    We use ExprMutator here, but really want a visitor as we don't
-    care about the resulting Expr.
-    """
-
-    def __init__(self, mod):
-        self.live_set = set()
-        self.mod = mod
-        super().__init__()
-
-    def visit_global_var(self, var):
-        if var not in self.live_set:
-            self.live_set.add(var)
-            self.visit(self.mod[var])
-        return var
-
-    def visit_if(self, ite):
-        return relay.If(
-            self.visit(ite.cond),
-            self.visit(ite.true_branch),
-            self.visit(ite.false_branch))
-
-
-def _live_from_main(mod):
-    ls = _LiveSet(mod)
-    ls.visit(mod[mod.entry_func])
-    return ls.live_set
-
-
-def _optimize_func(mod, func):
-    ck_expr = ir_pass.infer_type(func, mod=mod)
-    simp_expr = ir_pass.simplify_inference(ck_expr)
-    ck_simp = ir_pass.infer_type(simp_expr, mod=mod)
-    fused_expr = ir_pass.fuse_ops(ck_simp)
-    ck_fused = ir_pass.infer_type(fused_expr, mod=mod)
-    return ck_fused
 
 
 def _placeholder_body(type):
@@ -80,6 +40,20 @@ def build_module(funcs):
     return mod
 
 
+pass_set = transform.Sequential(
+    passes=[
+        transform.SimplifyInference(),
+        transform.CanonicalizeOps(),
+        transform.CanonicalizeCast(),
+        transform.FuseOps(3),
+        # transform.CombineParallelConv2d(),
+        transform.AlterOpLayout(),
+        # transform.RewriteAnnotatedOps(???),
+    ],
+    opt_level=0
+)
+
+
 def optimize(mod):
     """Optimize all the functions in a module.
 
@@ -87,6 +61,4 @@ def optimize(mod):
     optimization pass over the module which destructively updates each
     function while optimizing.
     """
-    ls = _live_from_main(mod)
-    for var in ls:
-        mod[var] = _optimize_func(mod, mod[var])
+    return pass_set(mod)
