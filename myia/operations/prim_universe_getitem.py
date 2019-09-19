07a242d1d2e7f586b444d701f475c49496a19d8b
Olivier Breuleux <breuleux@gmail.com>
2019-09-19 15:38:56 -0400

    fixup! Add handle, universe_getitem, universe_setitem

--- myia/operations/prim_universe_getitem.py
+++ myia/operations/prim_universe_getitem.py
@@ -1,7 +1,7 @@
 """Definitions for the primitive `universe_getitem`."""
 
 from .. import lib, xtype
-from ..lib import standard_prim
+from ..lib import broaden, standard_prim
 from . import primitives as P
 
 
@@ -15,7 +15,7 @@ async def infer_universe_getitem(self, engine,
                                  universe: xtype.UniverseType,
                                  handle: lib.AbstractHandle):
     """Infer the return type of primitive `universe_getitem`."""
-    return handle.element
+    return broaden(handle.element)
 
 
 __operation_defaults__ = {
