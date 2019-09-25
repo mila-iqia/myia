92b1b233502d8f3583feb2e4288d1234866a1d0a
Arnaud Bergeron <abergeron@gmail.com>
2019-09-25 12:11:13 -0400

    Attempt to fix coverage scripts

--- cov.sh
+++ cov.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 set -eux
 
-NAME=$1
-shift;
-
-"$@"
-
 if ls .coverage.* 1>/dev/null 2>&1; then
     coverage combine -a .coverage.*
 fi
 
-mv .coverage $NAME.coverage
+coverage report -m
