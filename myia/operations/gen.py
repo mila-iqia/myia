6ca04b4f43b8c8f2e65b8f0da4be883cf8fcb71e
Olivier Breuleux <breuleux@gmail.com>
2019-09-30 16:05:34 -0400

    Fixes

--- myia/operations/gen.py
+++ myia/operations/gen.py
@@ -1,10 +1,14 @@
-
+"""Utilities to generate methods."""
 
 from ..lib import core
 from . import hastype
 
 
 def lop(op, type, name):
+    """Generate a left hand side method.
+
+    Returns NotImplemented if the second input has the wrong type.
+    """
     @core(name=name, static_inline=True)
     def protocol(x, y):
         if hastype(y, type):
@@ -14,8 +18,11 @@ def lop(op, type, name):
     return protocol
 
 
-@core(static_inline=True)
 def rop(op, type, name):
+    """Generate a right hand side method.
+
+    Returns NotImplemented if the second input has the wrong type.
+    """
     @core(name=name, static_inline=True)
     def protocol(x, y):
         if hastype(y, type):
@@ -26,6 +33,7 @@ def rop(op, type, name):
 
 
 def reverse_binop(op, name):
+    """Reverse the argument order of a binary function."""
     @core(name=name, static_inline=True)
     def protocol(x, y):
         return op(y, x)
