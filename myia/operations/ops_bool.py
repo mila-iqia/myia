16542df622766b81c03f240d2522bf4a1c37041e
Olivier Breuleux <breuleux@gmail.com>
2019-09-30 15:21:20 -0400

    Use static_inline for several core functions

--- myia/operations/ops_bool.py
+++ myia/operations/ops_bool.py
@@ -6,14 +6,14 @@ from .utils import to_opdef
 
 
 @to_opdef
-@core
+@core(static_inline=True)
 def not_(x):
     """Implementation of `not`."""
     return operations.bool_not(bool(x))
 
 
 @to_opdef
-@core
+@core(static_inline=True)
 def bool_ne(x, y):
     """Implementation of `bool_ne`."""
     return operations.bool_not(operations.bool_eq(x, y))
--- myia/operations/ops_scalar.py
+++ myia/operations/ops_scalar.py
@@ -7,7 +7,7 @@ from .utils import to_opdef
 
 
 @to_opdef
-@core
+@core(static_inline=True)
 def int_floordiv(x, y):
     """Implementation of `int_floordiv`."""
     if (x <= 0) == (y <= 0):
@@ -17,7 +17,7 @@ def int_floordiv(x, y):
 
 
 @to_opdef
-@core
+@core(static_inline='inner')
 def int_truediv(x, y):
     """Implementation of `int_truediv`."""
     if hastype(x, typeof(y)):
@@ -30,21 +30,21 @@ def int_truediv(x, y):
 
 
 @to_opdef
-@core
+@core(static_inline=True)
 def float_floordiv(x, y):
     """Implementation of `float_floordiv`."""
     return floor(x / y)
 
 
 @to_opdef
-@core
+@core(static_inline=True)
 def int_bool(x):
     """Implementation of `int_bool`."""
     return x != 0
 
 
 @to_opdef
-@core
+@core(static_inline=True)
 def float_bool(x):
     """Implementation of `float_bool`."""
     return x != 0.0
