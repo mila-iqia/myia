b8ca5b4ff7e523ea253347d28aaf600200c56aa3
Olivier Breuleux <breuleux@gmail.com>
2020-04-08 13:24:08 -0400

    Fixes

--- scripts/install-deps-cpu.sh
+++ scripts/install-deps-cpu.sh
@@ -2,6 +2,5 @@ set -e
 
 poetry install
 python scripts/make-reqs.py custom.tool.conda.dependencies > requirements.conda
-python scripts/make-reqs.py custom.tool.conda.cpu-dependencies > requirements-cpu.conda
-conda install --file=requirements.conda
-conda install --file=requirements-cpu.conda
+python scripts/make-reqs.py custom.tool.conda.cpu-dependencies >> requirements.conda
+conda install -y --file=requirements.conda
--- scripts/install-deps-gpu.sh
+++ scripts/install-deps-gpu.sh
@@ -2,6 +2,5 @@ set -e
 
 poetry install
 python scripts/make-reqs.py custom.tool.conda.dependencies > requirements.conda
-python scripts/make-reqs.py custom.tool.conda.gpu-dependencies > requirements-gpu.conda
-conda install --file=requirements.conda
-conda install --file=requirements-gpu.conda
+python scripts/make-reqs.py custom.tool.conda.gpu-dependencies >> requirements.conda
+conda install -y --file=requirements.conda
