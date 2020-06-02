b0becbb532db78598047f781db6fe761285fd61c
Arnaud Bergeron <abergeron@gmail.com>
2020-06-02 16:38:57 -0400

    Make the ci install script work

--- scripts/install-deps-gpu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-poetry install
-python scripts/make-reqs.py custom.tool.conda.dependencies > requirements.conda
-python scripts/make-reqs.py custom.tool.conda.gpu-dependencies >> requirements.conda
-conda install -y --file=requirements.conda
