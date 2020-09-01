3bb2ee03d14adeb28eff492af3c5beaa3c0043bb
notoraptor <stevenbocco@gmail.com>
2020-09-01 00:09:21 -0400

    Make isort skip init files for backends.

--- myia_backend_pytorch/myia_backend_pytorch/__init__.py
+++ myia_backend_pytorch/myia_backend_pytorch/__init__.py
@@ -1,8 +1,12 @@
+"""Myia Pytorch backend.
+
+   isort:skip_file
+"""
 __version__ = "0.1.0"
 
+from .pytorch import load_backend, load_options
 from myia.testing.multitest import register_backend_testing
 
-from .pytorch import load_backend, load_options
 
 register_backend_testing("pytorch", "cpu", {"device": "cpu"})
 register_backend_testing("pytorch", "gpu", {"device": "cuda"}, "pytorch-cuda")
--- myia_backend_relay/myia_backend_relay/__init__.py
+++ myia_backend_relay/myia_backend_relay/__init__.py
@@ -1,8 +1,12 @@
+"""Myia Relay backend.
+
+   isort:skip_file
+"""
 __version__ = "0.1.0"
 
+from .relay import load_backend, load_options
 from myia.testing.multitest import register_backend_testing
 
-from .relay import load_backend, load_options
 
 register_backend_testing("relay", "cpu", {"target": "cpu", "device_id": 0})
 register_backend_testing(
