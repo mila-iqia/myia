85ca8fc9168c0b12a441fd95ead73773c1fc1119
Olivier Breuleux <breuleux@gmail.com>
2020-11-12 13:09:39 -0500

    Fix debugging stuff, add --bw flag to pytest

--- debug/gprint.py
+++ debug/gprint.py
@@ -1234,17 +1234,6 @@ class _AbstractScalar:
         )
 
 
-@mixin(abstract.AbstractFunction)
-class _AbstractFunction:
-    def __hrepr__(self, H, hrepr):
-        v = self.values[VALUE]
-        return hrepr.stdrepr_iterable(
-            v if isinstance(v, Possibilities) else [v],
-            before="★Function",
-            cls="abstract",
-        )
-
-
 @mixin(abstract.AbstractJTagged)
 class _AbstractJTagged:
     def __hrepr__(self, H, hrepr):
@@ -1344,7 +1333,7 @@ class _AbstractTuple:
         )
 
 
-@mixin(abstract.AbstractUnion)
+@mixin(abstract.AbstractChoices)
 class _AbstractUnion:
     def __hrepr__(self, H, hrepr):
         return hrepr.stdrepr_iterable(
