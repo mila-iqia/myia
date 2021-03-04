0e77ba78be247fd2afe910fa04b3266e9b6a7d39
Arnaud Bergeron <abergeron@gmail.com>
2021-03-04 14:41:36 -0500

    Give the ability to recursively print embedded graphs

--- myia/ir/print.py
+++ myia/ir/print.py
@@ -25,50 +25,66 @@ def _print_node(node, buf, nodecache, offset=0):
     print("", file=buf)
 
 
-def str_graph(g, allow_cycles=False):
+def str_graph(g, allow_cycles=False, recursive=True):
     nodecache = NodeCache()
     buf = io.StringIO()
-    print(f"graph {str(g)}(", file=buf, end="")
-    print(
-        ", ".join(
-            f"%{str(p)}{' : ' + str(p.astract) if p.abstract is not None else ''}"
-            for p in g.parameters
-            ),
-        file=buf, end="",
-        )
-
-    print(") ", file=buf, end="")
-    if g.return_.abstract is not None:
-        print(f"-> {g.return_.abstract} ", file=buf, end="")
-    print("{", file=buf)
-
     seen_nodes = set()
+    seen_graphs = set()
+    todo_graphs = [g]
 
     applies = []
+    first = True
 
-    todo = [g.return_]
-    while todo:
-        node = todo.pop()
-        if node in seen_nodes:
-            if allow_cycles:
-                break
-            else:
-                raise ValueError("cycle in sequence edges")
-        seen_nodes.add(node)
-        applies.append(node)
-        for edge in node.edges.values():
-            if (edge.node is not None and
-                    edge.node.is_apply() and
-                    edge.node.graph is g):
-                todo.append(edge.node)
-
-    for node in reversed(applies):
-        if (node.graph is not None and node.graph is not g) or node is g.return_:
+    while todo_graphs:
+        g = todo_graphs.pop()
+        if g in seen_graphs:
             continue
-        _print_node(node, buf, nodecache, offset=2)
+        seen_graphs.add(g)
 
-    print(f"  return {repr_node(g.output, nodecache)}", file=buf)
-    print("}", file=buf)
+        if not first:
+            print("", file=buf)
+        first = False
+
+        print(f"graph {str(g)}(", file=buf, end="")
+        print(
+            ", ".join(
+                f"%{str(p)}{' : ' + str(p.astract) if p.abstract is not None else ''}"
+                for p in g.parameters
+            ),
+            file=buf, end="",
+        )
+
+        print(") ", file=buf, end="")
+        if g.return_.abstract is not None:
+            print(f"-> {g.return_.abstract} ", file=buf, end="")
+        print("{", file=buf)
+
+        todo = [g.return_]
+        while todo:
+            node = todo.pop()
+            if node in seen_nodes:
+                if allow_cycles:
+                    continue
+                else:
+                    raise ValueError("cycle in sequence edges")
+            seen_nodes.add(node)
+            applies.append(node)
+            for edge in node.edges.values():
+                if edge.node is not None:
+                    if (edge.node.is_apply() and
+                            edge.node.graph is g):
+                        todo.append(edge.node)
+                    elif recursive and edge.node.is_constant_graph():
+                        todo_graphs.append(edge.node.value)
+
+        for node in reversed(applies):
+            if (node.graph is not None and node.graph is not g) or node is g.return_:
+                continue
+            _print_node(node, buf, nodecache, offset=2)
+
+        print(f"  return {repr_node(g.output, nodecache)}", file=buf)
+        print("}", file=buf)
+
     return buf.getvalue()
 
 
